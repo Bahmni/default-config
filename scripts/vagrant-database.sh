b4751c8c6017d810a95880f824c29ac6e5cedbcd
shruthidipali <shruthidipali@gmail.com>
2014-08-20 17:15:56 +0530

    Shruthi | Some basic config changes for possible

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -2,7 +2,7 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
-run_in_vagrant -c "sudo chown jss:jss /packages/build/possible_config/migrations"
-run_in_vagrant -c "sudo su - jss -c 'sudo cp -r /Project/possible-config/migrations/* /packages/build/possible_config/migrations/'"
-run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-implementation-liquibase.sh'"
-run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-implementation-openelis-config-liquibase.sh'"
+run_in_vagrant -c "sudo chown bahmni:bahmni /packages/build/possible_config/migrations"
+run_in_vagrant -c "sudo su - bahmni -c 'sudo cp -r /Project/possible-config/migrations/* /packages/build/possible_config/migrations/'"
+run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-liquibase.sh'"
+run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-openelis-config-liquibase.sh'"
