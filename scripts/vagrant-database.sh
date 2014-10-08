d99b01c841f7093f4515dc830aeebe877d33a34d
Deepak N <endeep123@gmail.com>
2014-10-08 17:24:01 +0530

    Fix vagrant database script

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -2,7 +2,12 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
-run_in_vagrant -c "sudo chown bahmni:bahmni /packages/build/possible_config/migrations"
-run_in_vagrant -c "sudo su - bahmni -c 'sudo cp -r /Project/possible-config/migrations/* /packages/build/possible_config/migrations/'"
-run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-liquibase.sh'"
-run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-openelis-config-liquibase.sh'"
+IMPLEMENTATION_NAME=possible
+APP_NAME=${1:-openmrs}
+MIGRATIONS_DIR="/packages/build/${IMPLEMENTATION_NAME}_config/${APP_NAME}/migrations"
+SRC_MIGRATIONS_DIR="/Project/${IMPLEMENTATION_NAME}-config/${APP_NAME}/migrations"
+
+run_in_vagrant -c "sudo rm -rf ${MIGRATIONS_DIR}/*"
+run_in_vagrant -c "sudo mkdir -p ${MIGRATIONS_DIR}"
+run_in_vagrant -c "sudo cp  ${SRC_MIGRATIONS_DIR}/* ${MIGRATIONS_DIR}/"
+run_in_vagrant -c "sudo /bahmni_temp/run-implementation-${APP_NAME}-liquibase.sh"
