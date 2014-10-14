158da7cad39b5dac8edc3fdb7c9494fb77e578d2
Arun Paul <arunpaul@thoughtworks.com>
2014-10-14 11:44:16 +0530

    Improved the query for number_of_clients

--- openmrs/reports/number_of_clients.sql
+++ openmrs/reports/number_of_clients.sql
@@ -6,26 +6,27 @@ SET @end_date = '2014-11-01';
 SET @report_group_name = 'Client Service Reports';
 
 -- Query
-Explain
 SELECT client_visits.age_group AS Age_Group,
-	IF(client_visits.age_group_id IS NULL, 0, SUM(IF(client_visits.first_visit_date BETWEEN @start_date AND @end_date,IF(client_visits.patient_gender = 'F', 1, 0),0))) AS New_Clients_Female,
-	IF(client_visits.age_group_id IS NULL, 0, SUM(IF(client_visits.first_visit_date BETWEEN @start_date AND @end_date,IF(client_visits.patient_gender = 'M', 1, 0),0))) AS New_Clients_Male,
-	IF(client_visits.age_group_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) as Total_Clients_Female,
-    IF(client_visits.age_group_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) as Total_Clients_Male
+	IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.first_visit_date BETWEEN @start_date AND @end_date,IF(client_visits.patient_gender = 'F', 1, 0),0))) AS New_Clients_Female,
+	IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.first_visit_date BETWEEN @start_date AND @end_date,IF(client_visits.patient_gender = 'M', 1, 0),0))) AS New_Clients_Male,
+	IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) as Total_Clients_Female,
+    IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) as Total_Clients_Male
 FROM    
-(SELECT DISTINCT patient.patient_id,
+(SELECT DISTINCT patient.patient_id AS patient_id,
 		observed_age_group.name AS age_group,
 		observed_age_group.id as age_group_id,
 		patient.date_created AS first_visit_date,
         person.gender AS patient_gender,
         observed_age_group.sort_order AS sort_order        
-FROM person
-INNER JOIN patient ON person.person_id = patient.patient_id
-LEFT OUTER JOIN visit_view AS visits ON patient.patient_id = visits.patient_id
-INNER JOIN possible_age_group AS observed_age_group ON observed_age_group.report_group_name = @report_group_name
-WHERE visits.date_started BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY)) 
-						AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY)) 
-AND visits.date_started BETWEEN @start_date AND @end_date )
-AS client_visits
+FROM visit
+INNER JOIN patient ON visit.patient_id = patient.patient_id AND visit.date_started BETWEEN @start_date AND @end_date
+INNER JOIN person ON person.person_id = patient.patient_id 
+RIGHT OUTER JOIN possible_age_group AS observed_age_group ON
+visit.date_started BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY)) 
+						AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+WHERE observed_age_group.report_group_name = @report_group_name) AS client_visits
 GROUP BY client_visits.age_group
 ORDER BY client_visits.sort_order;
+
+create index visit_date_started on visit(date_started);
+
