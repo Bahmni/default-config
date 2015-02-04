c2eebfce4aa503a49c64279e6163a54299ebd6db
Vinay Venu <vinayv@thoughtworks.com>
2015-02-04 12:41:19 +0530

    Add testing details for treatment adherence. Refactor query

--- openmrs/reports/ART_treatment_adherence.sql
+++ openmrs/reports/ART_treatment_adherence.sql
@@ -1,27 +1,32 @@
-SET @start_date = '2014-10-01';
-SET @end_date = '2015-02-01';
-select concept_id into @adh_a from concept_view where concept_full_name = 'Adherence Level A';
-select concept_id into @adh_b from concept_view where concept_full_name = 'Adherence Level B';
-select concept_id into @adh_c from concept_view where concept_full_name = 'Adherence Level C';
+SELECT
+  'Accessed' AS 'Treatment Adherence on ART',
+  count(*)   AS 'No of Cases'
+FROM obs_view
+WHERE concept_full_name = 'ART, ART adherence to ART' AND
+      value_coded IS NOT NULL AND obs_datetime < @end_date
 
-select 'Accessed' as 'Treatment Adherence on ART',
-	count(*) as 'No of Cases' from obs_view where concept_full_name = 'ART, ART adherence to ART' and 
-        value_coded is not null and obs_datetime < @end_date
+UNION ALL
 
-union all
+SELECT
+  '< 95% (A)' AS 'Treatment Adherence on ART',
+  count(*)    AS 'No of Cases'
+FROM valid_coded_obs_view
+WHERE concept_full_name = 'ART, ART adherence to ART'
+      AND obs_datetime < @end_date AND value_concept_full_name = 'Adherence Level A'
 
-select '< 95% (A)' as 'Treatment Adherence on ART',
-	count(*) as 'No of Cases' from obs_view where concept_full_name = 'ART, ART adherence to ART' and 
-        value_coded = @adh_a and obs_datetime < @end_date
+UNION ALL
 
-union all 
+SELECT
+  '80-95% (B)' AS 'Treatment Adherence on ART',
+  count(*)     AS 'No of Cases'
+FROM valid_coded_obs_view
+WHERE concept_full_name = 'ART, ART adherence to ART'
+      AND obs_datetime < @end_date AND value_concept_full_name = 'Adherence Level B'
 
-select '80-95% (B)' as 'Treatment Adherence on ART',
-	count(*) as 'No of Cases' from obs_view where concept_full_name = 'ART, ART adherence to ART' and 
-        value_coded = @adh_b and obs_datetime < @end_date
-
-union all 
-
-select '< 80% (C)' as 'Treatment Adherence on ART',
-	count(*) as 'No of Cases' from obs_view where concept_full_name = 'ART, ART adherence to ART' and 
-        value_coded = @adh_c and obs_datetime < @end_date
+UNION ALL
+SELECT
+  '< 80% (C)' AS 'Treatment Adherence on ART',
+  count(*)    AS 'No of Cases'
+FROM valid_coded_obs_view
+WHERE concept_full_name = 'ART, ART adherence to ART'
+      AND obs_datetime < @end_date AND value_concept_full_name = 'Adherence Level C';
