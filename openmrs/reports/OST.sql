5cf8bee25f829d6aed8712c48e9745b4b2133b7c
rohanpoddar <rohan.poddar@gmail.com>
2015-02-04 17:33:03 +0530

    Rohan | Making OST report into a single query

--- openmrs/reports/OST.sql
+++ openmrs/reports/OST.sql
@@ -1,20 +1,13 @@
-SET @start_date = '2014-10-01';
-SET @end_date = '2015-01-01';
-select concept_id into @positive from concept_view where concept_full_name = 'Positive';
-select concept_id into @negative from concept_view where concept_full_name = 'Negative';
-
-select concept_id into @ost_new from concept_view where concept_full_name = 'New';
-select concept_id into @ost_reg from concept_view where concept_full_name = 'Regular';
-select concept_id into @ost_transfer_in from concept_view where concept_full_name = 'Transfer In';
-
+SET @start_date = '2014-01-01';
+SET @end_date = '2014-01-25';
 
 select 'Clients on OST at the End of' as 'OST', 'Last' as 'Month',
        ifnull(sum(if(gender='F',1,0)),0) as 'Female',
        ifnull(sum(if(gender='M',1,0)),0) as 'Male',
        ifnull(sum(if(gender not in ('F','M'),1,0)),0) as 'TG' from
-       (select o.person_id, o.concept_full_name, o.value_text, o.value_coded, p.gender, o.obs_datetime from obs_view o
+       (select o.person_id, o.concept_full_name, o.value_coded, p.gender, o.obs_datetime from valid_coded_obs_view o
        	       inner join person p on o.person_id = p.person_id
-	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_coded is not null and
+	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_concept_full_name is not null and
 	       (o.obs_datetime < @start_date) group by person_id) as t1 
                     
 union all
@@ -23,9 +16,9 @@ select 'New Clients Enrolled at' as 'OST', 'This' as 'Month',
        ifnull(sum(if(gender='F',1,0)),0) as 'Female',
        ifnull(sum(if(gender='M',1,0)),0) as 'Male',
        ifnull(sum(if(gender not in ('F','M'),1,0)),0) as 'TG' from
-       (select o.person_id, o.concept_full_name, o.value_text,o.value_coded, p.gender, o.obs_datetime from obs_view o
+       (select o.person_id, o.concept_full_name,o.value_coded, p.gender, o.obs_datetime from valid_coded_obs_view o
        	       inner join person p on o.person_id = p.person_id
-	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_coded = @ost_new and   
+	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_concept_full_name = 'New' and
 	       (o.obs_datetime between @start_date and @end_date) group by person_id) as t2
 
 union all
@@ -34,9 +27,9 @@ select 'Clients Re-entry at' as 'OST', 'This' as 'Month',
        ifnull(sum(if(gender='F',1,0)),0) as 'Female',
        ifnull(sum(if(gender='M',1,0)),0) as 'Male',
        ifnull(sum(if(gender not in ('F','M'),1,0)),0) as 'TG' from
-       (select o.person_id, o.concept_full_name, o.value_text, o.value_coded, p.gender, o.obs_datetime from obs_view o
+       (select o.person_id, o.concept_full_name, o.value_coded, p.gender, o.obs_datetime from valid_coded_obs_view o
 	       inner join person p on o.person_id = p.person_id
-	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_coded = @ost_reg and   
+	       where o.concept_full_name = 'OST, Enrollment Type' and o.value_concept_full_name = 'Regular' and
 	       (o.obs_datetime between @start_date and @end_date) group by person_id) as t3
 
 union all
@@ -53,7 +46,9 @@ select 'Clients +ve Discharge at' as 'OST', 'This' as 'Month',
 		inner join
                 (select p.person_id as 'Person_ID' from obs_view o
 			inner join person p on o.person_id = p.person_id
-			where ((o.concept_full_name = 'HCT, Result if tested' and o.value_coded in (@positive)) or (o.concept_full_name='HIV Tridot' and o.value_text in ('Positive')))
+			where ((o.concept_full_name = 'HCT, Result if tested' and o.value_coded in (select concept_id from concept_view where concept_full_name = 'Positive')) or
+			        (o.concept_full_name='HIV (Serum)' and o.value_text in ('Positive')) or
+			        (o.concept_full_name='HIV (Blood)' and o.value_text in ('Positive')))
                     	and (o.obs_datetime between @start_date and @end_date) group by p.person_id) as t4_2 on t4_1.person_id=t4_2.Person_ID) as t4
                     
 union all
@@ -73,7 +68,7 @@ select 'Clients on OST at the End of' as 'OST', 'This' as 'Month',
        ifnull(sum(if(gender='F',1,0)),0) as 'Female',
        ifnull(sum(if(gender='M',1,0)),0) as 'Male',
        ifnull(sum(if(gender not in ('F','M'),1,0)),0) as 'TG' from
-       (select o.person_id, o.concept_full_name, o.value_text, o.value_coded, p.gender, o.obs_datetime from obs_view o
+       (select o.person_id, o.concept_full_name, o.value_coded, p.gender, o.obs_datetime from valid_coded_obs_view o
 		inner join person p on o.person_id = p.person_id
-		where o.concept_full_name = 'OST, Enrollment Type' and o.value_coded is not null and  
+		where o.concept_full_name = 'OST, Enrollment Type' and o.value_concept_full_name is not null and
 		(o.obs_datetime <= @end_date) group by person_id) as t6
