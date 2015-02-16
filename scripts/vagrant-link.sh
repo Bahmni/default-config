9e94017efcd06938aa4e02e53a3f7d9f40265be0
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-02-16 12:42:14 +0530

    Banka | Fixing vagrant-link script

--- scripts/vagrant-link.sh
+++ scripts/vagrant-link.sh
@@ -2,7 +2,8 @@
 
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
+USER=jss
 
 run_in_vagrant -c "sudo rm -rf /var/www/bahmni_config"
-run_in_vagrant -c "sudo ln -s /default-config /var/www/bahmni_config"
-run_in_vagrant -c "sudo chown -h jss:jss /var/www/bahmni_config"
+run_in_vagrant -c "sudo ln -s /bahmni/default-config /var/www/bahmni_config"
+run_in_vagrant -c "sudo chown -h ${USER}:${USER} /var/www/bahmni_config"
