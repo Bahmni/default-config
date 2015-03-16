418948743f209dea67353c2bd3dabe7d850d50de
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-03-16 19:01:16 +0530

    BAnka | Neonate form: taking care of voided obs. Casting obs datetime as date to include endDate.

--- openmrs/reports/hmis/neonate_form.sql
+++ openmrs/reports/hmis/neonate_form.sql
@@ -13,11 +13,12 @@ from
         SUM(if(gestation_period_obs.value_numeric between 37 and 41, 1, 0)) as 37_41,
         SUM(if(gestation_period_obs.value_numeric >= 42, 1, 0)) as above41
     from obs outcome_of_delivery_obs
-        left outer join obs gestation_period_obs on outcome_of_delivery_obs.person_id = gestation_period_obs.person_id and  gestation_period_obs.concept_id = (select concept_id from concept_name where name = 'Delivery Note, Gestation period' and concept_name_type = 'FULLY_SPECIFIED')
+        left outer join obs gestation_period_obs on outcome_of_delivery_obs.person_id = gestation_period_obs.person_id and  gestation_period_obs.concept_id = (select concept_id from concept_name where name = 'Delivery Note, Gestation period' and concept_name_type = 'FULLY_SPECIFIED') and gestation_period_obs.voided = false
         inner join concept_name outcome_value on outcome_value.concept_id = outcome_of_delivery_obs.value_coded and outcome_value.concept_name_type = 'FULLY_SPECIFIED'
     where
         outcome_of_delivery_obs.concept_id = (select concept_id from concept_name where name = "Delivery Note, Outcome of Delivery" and concept_name_type="FULLY_SPECIFIED")
-        and outcome_of_delivery_obs.obs_datetime between "#startDate#" and "#endDate#"
+        and CAST(outcome_of_delivery_obs.obs_datetime as DATE) between "#startDate#" and "#endDate#"
+        and outcome_of_delivery_obs.voided = false
     group by outcome_value.name) simpler_form
 group by delivery_outcome
 order by sort_order;
