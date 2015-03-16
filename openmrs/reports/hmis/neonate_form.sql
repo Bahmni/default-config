8cb3b1adf78165848987206ded15125508aefa79
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-03-16 17:01:03 +0530

    Banka | Removing usage of obs_view and coded_obs_view from neonate form custom sql.

--- openmrs/reports/hmis/neonate_form.sql
+++ openmrs/reports/hmis/neonate_form.sql
@@ -6,17 +6,18 @@ select
     SUM(above41) as ">= 42"
 from
     (select
-        IF(value_concept_full_name like 'Single%', "Primi", IF(value_concept_full_name like 'Twins%', "Multi", "Grand Multi")) as delivery_outcome,
-        IF(value_concept_full_name like 'Single%', 1, IF(value_concept_full_name like 'Twins%', 2, 3)) as sort_order,
-        SUM(if(obs_view.value_numeric between 22 and 27, 1, 0)) as 22_27,
-        SUM(if(obs_view.value_numeric between 28 and 36, 1, 0)) as 28_36,
-        SUM(if(obs_view.value_numeric between 37 and 41, 1, 0)) as 37_41,
-        SUM(if(obs_view.value_numeric >= 42, 1, 0)) as above41
-    from coded_obs_view
-        left outer join obs_view on obs_view.concept_full_name = "Delivery Note, Gestation period" and obs_view.person_id = coded_obs_view.person_id
+        IF(outcome_value.name like 'Single%', "Primi", IF(outcome_value.name like 'Twins%', "Multi", "Grand Multi")) as delivery_outcome,
+        IF(outcome_value.name like 'Single%', 1, IF(outcome_value.name like 'Twins%', 2, 3)) as sort_order,
+        SUM(if(gestation_period_obs.value_numeric between 22 and 27, 1, 0)) as 22_27,
+        SUM(if(gestation_period_obs.value_numeric between 28 and 36, 1, 0)) as 28_36,
+        SUM(if(gestation_period_obs.value_numeric between 37 and 41, 1, 0)) as 37_41,
+        SUM(if(gestation_period_obs.value_numeric >= 42, 1, 0)) as above41
+    from obs outcome_of_delivery_obs
+        left outer join obs gestation_period_obs on outcome_of_delivery_obs.person_id = gestation_period_obs.person_id and  gestation_period_obs.concept_id = (select concept_id from concept_name where name = 'Delivery Note, Gestation period' and concept_name_type = 'FULLY_SPECIFIED')
+        inner join concept_name outcome_value on outcome_value.concept_id = outcome_of_delivery_obs.value_coded and outcome_value.concept_name_type = 'FULLY_SPECIFIED'
     where
-        coded_obs_view.concept_full_name = "Delivery Note, Outcome of Delivery"
-        and coded_obs_view.obs_datetime between "#startDate#" and "#endDate#"
-    group by coded_obs_view.value_concept_full_name) simpler_form
+        outcome_of_delivery_obs.concept_id = (select concept_id from concept_name where name = "Delivery Note, Outcome of Delivery" and concept_name_type="FULLY_SPECIFIED")
+        and outcome_of_delivery_obs.obs_datetime between "#startDate#" and "#endDate#"
+    group by outcome_value.name) simpler_form
 group by delivery_outcome
 order by sort_order;
