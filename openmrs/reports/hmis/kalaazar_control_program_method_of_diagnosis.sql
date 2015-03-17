fde9fd47bcf86705f2476ecbb47a2f3c64a31369
bharatak <bharatak@thoughtworks.com>
2015-03-17 15:22:23 +0530

    Bharat,Charles| Custom Sql for Kala-azar Control Program  - Method of Diagnosis

---
+++ openmrs/reports/hmis/kalaazar_control_program_method_of_diagnosis.sql
@@ -0,0 +1,16 @@
+select base.result,IF(actual.number is null, 0, actual.number) as count from
+(SELECT 'Within' as result
+UNION
+    SELECT 'Outside' as result
+UNION
+    SELECT 'Foreigner' as result) base
+LEFT OUTER JOIN
+(SELECT
+  IF(pa.country = 'Nepal', IF(pa.county_district = 'Achham', 'Within', 'Outside'), 'Foreigner') result,
+  COUNT(pa.county_district)  as number
+FROM obs obs
+  INNER JOIN concept_name cn ON obs.concept_id = cn.concept_id
+  INNER JOIN person_address pa ON obs.person_id = pa.person_id
+WHERE cn.name = 'K-39' AND concept_name_type = 'FULLY_SPECIFIED' AND obs.value_text = 'Positive' 
+AND obs_datetime between '#startDate#' AND '#endDate#'
+GROUP BY result) actual on base.result = actual.result;
