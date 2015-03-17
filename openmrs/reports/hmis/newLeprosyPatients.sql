18273052471fe2e39c59be5b6371fa90a74f75ac
Divya <divyad@INdivyad.local>
2015-03-17 11:29:45 +0530

    Preethi, Divya | Handled leprosy patients with no type or grade specified and age for < 1 year

--- openmrs/reports/hmis/newLeprosyPatients.sql
+++ openmrs/reports/hmis/newLeprosyPatients.sql
@@ -1,30 +1,44 @@
-select
-@a:=@a+1 as SN,
-pa.person_id as 'Person Id',
-concat(given_name, " ", family_name) as 'Name of New Cases (Patient)',
-cv1.concept_full_name as 'Caste Code',
-cast(DATEDIFF(CURRENT_DATE, person.birthdate)/365 as decimal(10, 0)) AS Age,
-gender as Gender,
-county_district as District,
-city_village as 'VDC/Municipality',
-paddr.address1 as 'Ward No',
--- work around to resolve the conflict in jasper
-substring(cv4.concept_full_name, 1, length(cv4.concept_full_name)) as Type,
-substring(cv6.concept_full_name, 1, length(cv6.concept_full_name)) as DG
-from (select @a:=0) initvars, person_name
-join person on person.person_id = person_name.person_id
-join person_address as paddr on paddr.person_id = person.person_id
-join person_attribute as pa on pa.person_id = person.person_id
-join concept_view cv1 on pa.value = cv1.concept_id
+SELECT
+  @a := @a + 1                                                               AS SN,
+  pa.person_id                                                               AS 'Person Id',
+  concat(given_name, " ", family_name)                                       AS 'Name of New Cases (Patient)',
+  cv1.concept_full_name                                                      AS 'Caste Code',
+  if(DATEDIFF(CURRENT_DATE, person.birthdate) / 365 < 1,
+     cast(DATEDIFF(CURRENT_DATE, person.birthdate) / 365 AS DECIMAL(10, 1)),
+     cast(DATEDIFF(CURRENT_DATE, person.birthdate) / 365 AS DECIMAL(10, 0))) AS Age,
+  gender                                                                     AS Gender,
+  county_district                                                            AS District,
+  city_village                                                               AS 'VDC/Municipality',
+  paddr.address1                                                             AS 'Ward No',
+  leprosy_type.type                                                          AS Type,
+  grade.grade_type                                                           AS DG
 
-join concept_view cv3 on cv3.concept_full_name ='Leprosy, Leprosy Type'
-join obs as lepTypeObs on  pa.person_id = lepTypeObs.person_id and lepTypeObs.concept_id = cv3.concept_id
-join concept_view cv4 on cv4.concept_id = lepTypeObs.value_coded
+FROM (SELECT @a := 0) initvars, person_name
+  JOIN person ON person.person_id = person_name.person_id
+  JOIN person_address AS paddr ON paddr.person_id = person.person_id
+  JOIN person_attribute AS pa ON pa.person_id = person.person_id
+  JOIN concept_view cv1 ON pa.value = cv1.concept_id
 
-join obs as dgObs on  lepTypeObs.obs_group_id = dgObs.obs_group_id and  lepTypeObs.person_id = dgObs.person_id 
-join concept_view cv5 on dgObs.concept_id = cv5.concept_id and cv5.concept_full_name ='Leprosy, Disability Grade'
-join concept_view cv6 on dgObs.value_coded=cv6.concept_id
-
-where pa.person_id in (select newPatientObs.person_id  from obs as newPatientObs
-	join concept_view cv2 on newPatientObs.value_coded = cv2.concept_id and cv2.concept_full_name='New Patients'
-	and cast(newPatientObs.obs_datetime AS DATE) BETWEEN '#startDate#' and '#endDate#');
+  JOIN obs AS newPatientObs ON person.person_id = newPatientObs.person_id
+  JOIN concept_view cv2 ON newPatientObs.value_coded = cv2.concept_id AND cv2.concept_full_name = 'New Patients'
+                           AND cast(newPatientObs.obs_datetime AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+  LEFT JOIN
+  (SELECT
+     person_id,
+     obs_group_id,
+     cv4.concept_full_name AS type
+   FROM obs AS lepTypeObs
+     JOIN concept_view cv3 ON lepTypeObs.concept_id = cv3.concept_id AND cv3.concept_full_name = 'Leprosy, Leprosy Type'
+     JOIN concept_view cv4 ON cv4.concept_id = lepTypeObs.value_coded) AS leprosy_type
+    ON leprosy_type.person_id = person.person_id AND
+       newPatientObs.obs_group_id = leprosy_type.obs_group_id
+  LEFT JOIN
+  (SELECT
+     person_id,
+     obs_group_id,
+     cv6.concept_full_name AS grade_type
+   FROM obs AS dgObs
+     JOIN concept_view cv5 ON dgObs.concept_id = cv5.concept_id AND cv5.concept_full_name = 'Leprosy, Disability Grade'
+     JOIN concept_view cv6 ON dgObs.value_coded = cv6.concept_id) AS grade
+    ON newPatientObs.obs_group_id = grade.obs_group_id AND
+       person.person_id = grade.person_id;
