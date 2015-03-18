45d70332cd280943d71dcbc42fe726c1794488c0
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-03-18 10:56:47 +0530

    Banka | Fixing reporting age group name in inpatient outcome death sql.

--- openmrs/reports/hmis/inpatient_outcome_death.sql
+++ openmrs/reports/hmis/inpatient_outcome_death.sql
@@ -9,6 +9,6 @@ from
 LEFT OUTER JOIN person on (person.death_date BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL age_group.min_years YEAR), INTERVAL age_group.min_days DAY)) AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL age_group.max_years YEAR), INTERVAL age_group.max_days DAY))) and person.death_date is not null
 LEFT OUTER JOIN (select max(encounter_datetime) as admission_datetime, patient_id from encounter_view where encounter_type_name='ADMISSION' group by patient_id) last_admission on person.person_id = last_admission.patient_id and last_admission.admission_datetime BETWEEN '#startDate#' and '#endDate#'
 where
-	age_group.report_group_name = 'Inpatient Discharge Reports'
+	age_group.report_group_name = 'Inpatient'
 group by age_group.name
 order by age_group.sort_order;
