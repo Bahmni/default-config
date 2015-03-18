ea0225ccbbea5604b2d524152716749e1d1f08bf
arunpaul <arunpaul@INarunpaul1.local>
2015-03-18 12:08:05 +0530

    changes to birth_weight query

--- openmrs/reports/hmis/birth_weight.sql
+++ openmrs/reports/hmis/birth_weight.sql
@@ -6,11 +6,11 @@ FROM
 (SELECT obs_weight.value_numeric as weight,
  obs_defect.value_coded as defect,
  obs_asphyxia.value_concept_full_name as asphyxia,
- possible_weight_group.name, possible_weight_group.sort_order as sort_order
+ reporting_age_group.name, reporting_age_group.sort_order as sort_order
 FROM obs_view as obs_delivery_time
 INNER JOIN obs_view AS obs_weight ON obs_weight.encounter_id = obs_delivery_time.encounter_id
 	AND obs_delivery_time.concept_full_name = 'Delivery Note, Delivery date and time'
-    AND DATE(obs_delivery_time.value_datetime) BETWEEN @start_date AND @end_date
+    AND DATE(obs_delivery_time.value_datetime) BETWEEN '#startDate#' AND '#endDate#'
     AND obs_weight.concept_full_name = 'Delivery Note, Liveborn weight'
 LEFT OUTER JOIN obs_view AS obs_defect ON obs_weight.obs_group_id = obs_defect.obs_group_id
 		AND obs_defect.concept_full_name = 'Delivery Note, Liveborn defects present'
@@ -18,8 +18,10 @@ LEFT OUTER JOIN obs_view AS obs_defect ON obs_weight.obs_group_id = obs_defect.o
 LEFT OUTER JOIN coded_obs_view AS obs_asphyxia ON obs_weight.obs_group_id = obs_asphyxia.obs_group_id
 	AND obs_asphyxia.concept_full_name = 'Delivery Note, New Born Status'
 	AND obs_asphyxia.value_concept_full_name = 'Asphyxiated'
-RIGHT OUTER JOIN possible_weight_group ON
-	obs_weight.value_numeric >= possible_weight_group.min_weight AND obs_weight.value_numeric < possible_weight_group.max_weight
-WHERE possible_weight_group.report_group_name = 'Birth Weight Report') AS birth_weight
+RIGHT OUTER JOIN reporting_age_group ON
+	obs_weight.value_numeric >= reporting_age_group.min_years AND obs_weight.value_numeric < reporting_age_group.max_years
+WHERE reporting_age_group.report_group_name = 'Birth Weight Report') AS birth_weight
 GROUP BY birth_weight.name
-ORDER BY birth_weight.sort_order;    
+ORDER BY birth_weight.sort_order;   
+
+ 
