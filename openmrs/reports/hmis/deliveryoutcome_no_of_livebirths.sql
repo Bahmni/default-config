687b5b2e02686cc253b594d1f7dd0644e4a9343c
rohanpoddar <rohan.poddar@gmail.com>
2015-03-18 11:20:42 +0530

    Rohan, Arun  | Fixing the sql

--- openmrs/reports/hmis/deliveryoutcome_no_of_livebirths.sql
+++ openmrs/reports/hmis/deliveryoutcome_no_of_livebirths.sql
@@ -12,7 +12,7 @@ FROM coded_obs_view as obs_delivery_outcome
 INNER JOIN obs_view ON obs_delivery_outcome.obs_group_id = obs_view.obs_group_id
 	AND obs_delivery_outcome.concept_full_name = 'Delivery Note, Outcome of Delivery'
     AND obs_view.concept_full_name = 'Delivery Note, Delivery date and time'
- 	 AND DATE(obs_view.value_datetime) BETWEEN #startDate# AND #endDate#
+ 	 AND DATE(obs_view.value_datetime) BETWEEN '#startDate#' AND '#endDate#'
     AND obs_delivery_outcome.value_concept_full_name IN 
     ('Single - livebirth','Twins - both liveborn','Twins - one liveborn and one stillborn', 'Other multiple births - all liveborn','Other multiple births - some liveborn')
 LEFT OUTER JOIN coded_obs_view AS infant_gender ON obs_delivery_outcome.person_id = infant_gender.person_id  
