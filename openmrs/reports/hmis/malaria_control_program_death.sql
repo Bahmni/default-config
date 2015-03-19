56573cc55f46585fc2ae497a42576846a970f6c0
Mujir <mujir@thoughtworks.com>
2015-03-19 19:52:53 +0530

    Mujir | #000 | changed malaria program - death report sql

--- openmrs/reports/hmis/malaria_control_program_death.sql
+++ openmrs/reports/hmis/malaria_control_program_death.sql
@@ -1,32 +1,19 @@
 SELECT
-  disease_name.name,
-  SUM(IF(disease_name.name = disease.concept_full_name AND rag.name = '< 5 Years' AND person.person_id IS NOT NULL, 1, 0)) AS '< 5 Years',
-  SUM(IF(disease_name.name = disease.concept_full_name AND rag.name = '≥ 5 Years' and person.person_id IS NOT NULL, 1, 0)) AS '≥ 5 Years'
+  disease_name.name as disease,
+  rag.name as age_category,
+  SUM(IF(disease_name.name = answer.name AND person.person_id IS NOT NULL, 1, 0)) AS count
 FROM
   (SELECT 'Plasmodium Vivax' name UNION SELECT 'Plasmodium Falciparum' name) AS disease_name
-  JOIN reporting_age_group rag
-    ON rag.report_group_name = 'Malaria'
-  JOIN concept_view death_note_concept
-    ON death_note_concept.concept_full_name = 'Death Note'
-  LEFT JOIN obs death_note_obs
-    ON death_note_concept.concept_id = death_note_obs.concept_id
-    AND death_note_obs.obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+  JOIN reporting_age_group rag ON rag.report_group_name = 'Malaria'
+  JOIN concept_view question ON question.concept_full_name in
+                                              ('Death Note, PRIMARY Cause of Death', 'Death Note, Secondary Cause of Death', 'Death Note, Tertiary Cause of Death')
+  JOIN concept_name answer on answer.name = disease_name.name AND answer.concept_name_type = 'FULLY_SPECIFIED'
+  LEFT JOIN obs obs ON obs.concept_id = question.concept_id
+       AND obs.value_coded = answer.concept_id
+       AND date(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
+       AND obs.voided=0
+  LEFT JOIN person person ON person.person_id = obs.person_id
+       AND CAST(obs.obs_datetime AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY))
+       AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
+GROUP BY disease_name.name, rag.name;
 
-  LEFT JOIN obs death_date_obs
-    ON death_date_obs.obs_group_id = death_note_obs.obs_id
-
-  LEFT JOIN person person
-    ON person.person_id = death_note_obs.person_id
-    AND CAST(death_date_obs.value_datetime AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY))
-    AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
-
-  LEFT JOIN obs primary_cause_obs
-    ON death_note_obs.obs_id = primary_cause_obs.obs_group_id
-  LEFT JOIN concept_view primary_cause_concept
-    ON primary_cause_concept.concept_id = primary_cause_obs.concept_id
-    AND primary_cause_concept.concept_full_name IN
-           ('Death Note, Primary Cause of Death', 'Death Note, Secondary Cause of Death', 'Death Note, Tertiary Cause of Death')
-  LEFT JOIN concept_view disease
-    ON disease.concept_id = primary_cause_obs.value_coded
-    AND disease.concept_full_name = disease_name.name
-GROUP BY disease_name.name;
