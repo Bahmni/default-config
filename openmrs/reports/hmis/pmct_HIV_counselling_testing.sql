a0ffd40d03a002d713b2ebe82ca7347e5efaa7b0
bharatak <bharatak@thoughtworks.com>
2015-03-19 11:12:12 +0530

    Charles, Bharat| Custom Sql for PMTCT of HIV - Counselling and Testing

--- openmrs/reports/hmis/pmct_HIV_counselling_testing.sql
+++ openmrs/reports/hmis/pmct_HIV_counselling_testing.sql
@@ -1,55 +1,59 @@
-SELECT IF(cn.name = 'ANC, HIV Counseling', cn.name, 'ANC, HIV Result Received') result, count(distinct person_id)
+SELECT IF(cn.name = 'ANC, HIV Counseling', cn.name, 'ANC, HIV Result Received') result, count(distinct person_id) as Count
 FROM obs obs
   INNER JOIN concept_name cn ON obs.concept_id = cn.concept_id AND obs.voided = 0 and cn.name in ('ANC, HIV Counseling','ANC, HIV Result Received') and cn.concept_name_type = 'FULLY_SPECIFIED'
   INNER JOIN concept_name trueConcept
     ON obs.value_coded = trueConcept.concept_id AND trueConcept.concept_name_type = 'FULLY_SPECIFIED' and trueConcept.name = 'True'
+WHERE DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
 GROUP BY result
 UNION
-SELECT 'ANC, HIV Test Result' as result1, count(distinct person_id)
+SELECT 'ANC, HIV Test Result' as result, count(distinct person_id) AS Count
 FROM obs obs
   INNER JOIN concept_name cn ON obs.concept_id = cn.concept_id AND obs.voided = 0 and cn.name = 'ANC, HIV Test Result' and cn.concept_name_type = 'FULLY_SPECIFIED'
   INNER JOIN concept_name trueConcept
     ON obs.value_coded = trueConcept.concept_id AND trueConcept.concept_name_type = 'FULLY_SPECIFIED' and trueConcept.name ='Positive'
-GROUP BY result1
+WHERE DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
+GROUP BY result
 UNION
-SELECT 'HTC, Pre-test Counseling' as result2, count(distinct obs.person_id)
-FROM obs obs
-  INNER JOIN person on person.person_id = obs.person_id
+SELECT 'Labour and delivery - Counseled' as result, count(distinct person.person_id) AS Count
+FROM person person
+  INNER JOIN obs on obs.person_id = person.person_id
   INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
-  INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED' 
+  INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
   INNER JOIN concept_name cn ON obs.concept_id = cn.concept_id AND obs.voided = 0 and cn.name = 'HTC, Pre-test Counseling' and cn.concept_name_type = 'FULLY_SPECIFIED'
   INNER JOIN concept_name trueConcept
     ON obs.value_coded = trueConcept.concept_id AND trueConcept.concept_name_type = 'FULLY_SPECIFIED' and trueConcept.name ='True'
-GROUP BY result2
+WHERE DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
+GROUP BY result
 UNION
-SELECT 'Labour and Delivery - Tested' as result,count(distinct person.person_id)
-FROM obs obs
-  INNER JOIN person on person.person_id = obs.person_id and obs.voided = 0
+SELECT 'Labour and Delivery - Tested' as result, count(distinct person.person_id) AS Count
+FROM person person
+  INNER JOIN obs on person.person_id = obs.person_id and obs.voided = 0
   INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
   INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
   LEFT JOIN
-  (select testedBeforeObs.obs_id 
-        from obs testedBeforeObs 
-          INNER JOIN concept_name testedBeforeCname ON testedBeforeObs.concept_id = testedBeforeCname.concept_id AND testedBeforeObs.voided = 0 and testedBeforeCname.name = 'HTC, Tested before' and testedBeforeCname.concept_name_type = 'FULLY_SPECIFIED'
-          INNER JOIN concept_name testedBeforeYes on testedBeforeObs.value_coded = testedBeforeYes.concept_id and testedBeforeYes.name ='True' and testedBeforeYes.concept_name_type = 'FULLY_SPECIFIED') testedBefore on testedBefore.obs_id = obs.obs_id
-  
+  (select testedBeforeObs.obs_id
+   from obs testedBeforeObs
+     INNER JOIN concept_name testedBeforeCname ON testedBeforeObs.concept_id = testedBeforeCname.concept_id AND testedBeforeObs.voided = 0 and testedBeforeCname.name = 'HTC, Tested before' and testedBeforeCname.concept_name_type = 'FULLY_SPECIFIED'
+     INNER JOIN concept_name testedBeforeYes on testedBeforeObs.value_coded = testedBeforeYes.concept_id and testedBeforeYes.name ='True' and testedBeforeYes.concept_name_type = 'FULLY_SPECIFIED') testedBefore on testedBefore.obs_id = obs.obs_id
+
   LEFT JOIN
   (select htcConfirmatoryObs.obs_id
    from obs htcConfirmatoryObs
      INNER JOIN concept_name htcConfirmatoryCname ON htcConfirmatoryObs.concept_id = htcConfirmatoryCname.concept_id AND htcConfirmatoryObs.voided = 0 and htcConfirmatoryCname.name = 'HTC, Confirmatory' and htcConfirmatoryCname.concept_name_type = 'FULLY_SPECIFIED' and htcConfirmatoryObs.value_coded is not null) htcConfirmatory on htcConfirmatory.obs_id = obs.obs_id
-  LEFT JOIN 
+  LEFT JOIN
   (select htcInitialObs.obs_id
-    from obs htcInitialObs
-      INNER JOIN concept_name htcInitialCname ON htcInitialObs.concept_id = htcInitialCname.concept_id AND htcInitialObs.voided = 0 and htcInitialCname.name = 'HTC, Initial' and htcInitialCname.concept_name_type = 'FULLY_SPECIFIED' and htcInitialObs.value_coded is not null) htcInitial on htcInitial.obs_id = obs.obs_id
+   from obs htcInitialObs
+     INNER JOIN concept_name htcInitialCname ON htcInitialObs.concept_id = htcInitialCname.concept_id AND htcInitialObs.voided = 0 and htcInitialCname.name = 'HTC, Initial' and htcInitialCname.concept_name_type = 'FULLY_SPECIFIED' and htcInitialObs.value_coded is not null) htcInitial on htcInitial.obs_id = obs.obs_id
   LEFT JOIN
   (select htcTieBreakerObs.obs_id
    from obs htcTieBreakerObs
      INNER JOIN concept_name htcTieBreakerCname ON htcTieBreakerObs.concept_id = htcTieBreakerCname.concept_id AND htcTieBreakerObs.voided = 0 and htcTieBreakerCname.name = 'HTC, Tie Breaker' and htcTieBreakerCname.concept_name_type = 'FULLY_SPECIFIED' and htcTieBreakerObs.value_coded is not null) htcTieBreaker on htcTieBreaker.obs_id = obs.obs_id
- where testedBefore.obs_id is not null or htcConfirmatory.obs_id is not null or htcInitial.obs_id is not null or htcTieBreaker.obs_id is not null
+where (testedBefore.obs_id is not null or htcConfirmatory.obs_id is not null or htcInitial.obs_id is not null or htcTieBreaker.obs_id is not null)
+                                                                                                                AND DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
 UNION
-SELECT 'Labour and Delivery - Positive' as result,count(distinct person.person_id)
-FROM obs obs
-  INNER JOIN person on person.person_id = obs.person_id and obs.voided = 0
+SELECT 'Labour and Delivery - Positive' as result, count(distinct person.person_id) AS Count
+FROM person person
+  INNER JOIN obs on person.person_id = obs.person_id and obs.voided = 0
   INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
   INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
   LEFT JOIN
@@ -57,7 +61,6 @@ FROM obs obs
    from obs resultIfTestedObs
      INNER JOIN concept_name resultIfTestedCname ON resultIfTestedObs.concept_id = resultIfTestedCname.concept_id AND resultIfTestedObs.voided = 0 and resultIfTestedCname.name = 'HTC, Result if tested' and resultIfTestedCname.concept_name_type = 'FULLY_SPECIFIED'
      INNER JOIN concept_name resultIfTestedPositive on resultIfTestedObs.value_coded = resultIfTestedPositive.concept_id and resultIfTestedPositive.name ='Positive' and resultIfTestedPositive.concept_name_type = 'FULLY_SPECIFIED') resultIfTested on resultIfTested.obs_id = obs.obs_id
-
   LEFT JOIN
   (select htcConfirmatoryObs.obs_id
    from obs htcConfirmatoryObs
@@ -73,11 +76,12 @@ FROM obs obs
    from obs htcTieBreakerObs
      INNER JOIN concept_name htcTieBreakerCname ON htcTieBreakerObs.concept_id = htcTieBreakerCname.concept_id AND htcTieBreakerObs.voided = 0 and htcTieBreakerCname.name = 'HTC, Tie Breaker' and htcTieBreakerCname.concept_name_type = 'FULLY_SPECIFIED'
      INNER JOIN concept_name htcTieBreakerPositive on htcTieBreakerObs.value_coded = htcTieBreakerPositive.concept_id and htcTieBreakerPositive.name ='Positive' and htcTieBreakerPositive.concept_name_type = 'FULLY_SPECIFIED') htcTieBreaker on htcTieBreaker.obs_id = obs.obs_id
-where (resultIfTested.obs_id is not null) or (htcConfirmatory.obs_id is not null and htcInitial.obs_id is not null) or (htcTieBreaker.obs_id is not null)
+where ((resultIfTested.obs_id is not null) or (htcConfirmatory.obs_id is not null and htcInitial.obs_id is not null) or (htcTieBreaker.obs_id is not null))
+                                                                                                                       AND DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
 UNION
-SELECT 'Puerperium - Counseled' as result,count(distinct person.person_id)
-FROM obs obs
-  INNER JOIN person on person.person_id = obs.person_id and obs.voided = 0
+SELECT 'Puerperium - Counseled' as result, count(distinct person.person_id) AS Count
+FROM person person
+  INNER JOIN obs on person.person_id = obs.person_id and obs.voided = 0
   INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
   INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
   LEFT JOIN
@@ -89,17 +93,18 @@ FROM obs obs
    from obs htcPretestCouncellingObs
      INNER JOIN concept_name htcPretestCouncellingCname ON htcPretestCouncellingObs.concept_id = htcPretestCouncellingCname.concept_id AND htcPretestCouncellingObs.voided = 0 and htcPretestCouncellingCname.name = 'HTC, Pre-test Counseling' and htcPretestCouncellingCname.concept_name_type = 'FULLY_SPECIFIED'
      INNER JOIN concept_name htcPretestCouncellingPositive on htcPretestCouncellingObs.value_coded = htcPretestCouncellingPositive.concept_id and htcPretestCouncellingPositive.name ='Positive' and htcPretestCouncellingPositive.concept_name_type = 'FULLY_SPECIFIED') htcPretestCouncelling on htcPretestCouncelling.obs_id = obs.obs_id
-where (pncVisitTime.obs_id is not null) or (htcPretestCouncelling.obs_id is not null)
+where ((pncVisitTime.obs_id is not null) or (htcPretestCouncelling.obs_id is not null))                                                                                                                 
+                                           AND DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
 UNION
-SELECT 'Puerperium - Tested' as result, count(distinct person.person_id)
+SELECT 'Puerperium - Tested' as result, count(distinct person.person_id) AS Count
 FROM person person
   INNER JOIN obs obs on person.person_id = obs.person_id and obs.voided = 0
   INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
   INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
-  LEFT JOIN 
+  LEFT JOIN
   (select pncVisitTimeObs.person_id, pncVisitTimeObs.obs_id
-    from obs pncVisitTimeObs
-    INNER JOIN concept_name pncVisitTimeCname ON pncVisitTimeObs.concept_id = pncVisitTimeCname.concept_id AND pncVisitTimeObs.voided = 0 and pncVisitTimeCname.name = 'PNC, Visit Time' and pncVisitTimeCname.concept_name_type = 'FULLY_SPECIFIED' and pncVisitTimeObs.value_coded is not null) pncVisitTime on pncVisitTime.person_id = obs.person_id
+   from obs pncVisitTimeObs
+     INNER JOIN concept_name pncVisitTimeCname ON pncVisitTimeObs.concept_id = pncVisitTimeCname.concept_id AND pncVisitTimeObs.voided = 0 and pncVisitTimeCname.name = 'PNC, Visit Time' and pncVisitTimeCname.concept_name_type = 'FULLY_SPECIFIED' and pncVisitTimeObs.value_coded is not null) pncVisitTime on pncVisitTime.person_id = obs.person_id
   LEFT JOIN
   (select testedBeforeObs.person_id,testedBeforeObs.obs_id
    from obs testedBeforeObs
@@ -117,4 +122,37 @@ FROM person person
   (select htcTieBreakerObs.person_id,htcTieBreakerObs.obs_id
    from obs htcTieBreakerObs
      INNER JOIN concept_name htcTieBreakerCname ON htcTieBreakerObs.concept_id = htcTieBreakerCname.concept_id AND htcTieBreakerObs.voided = 0 and htcTieBreakerCname.name = 'HTC, Tie Breaker' and htcTieBreakerCname.concept_name_type = 'FULLY_SPECIFIED' and htcTieBreakerObs.value_coded is not null) htcTieBreaker on htcTieBreaker.person_id = obs.person_id
-where (pncVisitTime.obs_id is not null) and (testedBefore.obs_id is not null or htcConfirmatory.obs_id is not null or htcInitial.obs_id is not null or htcTieBreaker.obs_id is not null)
+where ((pncVisitTime.obs_id is not null) and (testedBefore.obs_id is not null or htcConfirmatory.obs_id is not null or htcInitial.obs_id is not null or htcTieBreaker.obs_id is not null))
+      AND DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
+UNION
+SELECT 'Puerperium - Positive' as result, count(distinct person.person_id) as Count
+FROM person person
+  INNER JOIN obs obs on person.person_id = obs.person_id and obs.voided = 0
+  INNER JOIN obs deliveryMethodObs on deliveryMethodObs.person_id = person.person_id and deliveryMethodObs.voided = 0 and deliveryMethodObs.value_coded is not null
+  INNER JOIN concept_name deliveryMethodConcept ON deliveryMethodObs.concept_id = deliveryMethodConcept.concept_id and deliveryMethodConcept.name = 'Delivery Note, Method of Delivery' and deliveryMethodConcept.concept_name_type = 'FULLY_SPECIFIED'
+  LEFT JOIN
+  (select pncVisitTimeObs.person_id, pncVisitTimeObs.obs_id
+   from obs pncVisitTimeObs
+     INNER JOIN concept_name pncVisitTimeCname ON pncVisitTimeObs.concept_id = pncVisitTimeCname.concept_id AND pncVisitTimeObs.voided = 0 and pncVisitTimeCname.name = 'PNC, Visit Time' and pncVisitTimeCname.concept_name_type = 'FULLY_SPECIFIED' and pncVisitTimeObs.value_coded is not null) pncVisitTime on pncVisitTime.person_id = obs.person_id
+  LEFT JOIN
+  (select resultIfTestedObs.obs_id,resultIfTestedObs.person_id
+   from obs resultIfTestedObs
+     INNER JOIN concept_name resultIfTestedCname ON resultIfTestedObs.concept_id = resultIfTestedCname.concept_id AND resultIfTestedObs.voided = 0 and resultIfTestedCname.name = 'HTC, Result if tested' and resultIfTestedCname.concept_name_type = 'FULLY_SPECIFIED'
+     INNER JOIN concept_name resultIfTestedPositive on resultIfTestedObs.value_coded = resultIfTestedPositive.concept_id and resultIfTestedPositive.name ='Positive' and resultIfTestedPositive.concept_name_type = 'FULLY_SPECIFIED') resultIfTested on resultIfTested.person_id = obs.person_id
+  LEFT JOIN
+  (select htcConfirmatoryObs.obs_id
+   from obs htcConfirmatoryObs
+     INNER JOIN concept_name htcConfirmatoryCname ON htcConfirmatoryObs.concept_id = htcConfirmatoryCname.concept_id AND htcConfirmatoryObs.voided = 0 and htcConfirmatoryCname.name = 'HTC, Confirmatory' and htcConfirmatoryCname.concept_name_type = 'FULLY_SPECIFIED'
+     INNER JOIN concept_name htcConfirmatoryPositive on htcConfirmatoryObs.value_coded = htcConfirmatoryPositive.concept_id and htcConfirmatoryPositive.name ='Positive' and htcConfirmatoryPositive.concept_name_type = 'FULLY_SPECIFIED') htcConfirmatory on htcConfirmatory.obs_id = obs.obs_id
+  LEFT JOIN
+  (select htcInitialObs.obs_id
+   from obs htcInitialObs
+     INNER JOIN concept_name htcInitialCname ON htcInitialObs.concept_id = htcInitialCname.concept_id AND htcInitialObs.voided = 0 and htcInitialCname.name = 'HTC, Initial' and htcInitialCname.concept_name_type = 'FULLY_SPECIFIED'
+     INNER JOIN concept_name htcInitialPositive on htcInitialObs.value_coded = htcInitialPositive.concept_id and htcInitialPositive.name ='Positive' and htcInitialPositive.concept_name_type = 'FULLY_SPECIFIED') htcInitial on htcInitial.obs_id = obs.obs_id
+  LEFT JOIN
+  (select htcTieBreakerObs.obs_id
+   from obs htcTieBreakerObs
+     INNER JOIN concept_name htcTieBreakerCname ON htcTieBreakerObs.concept_id = htcTieBreakerCname.concept_id AND htcTieBreakerObs.voided = 0 and htcTieBreakerCname.name = 'HTC, Tie Breaker' and htcTieBreakerCname.concept_name_type = 'FULLY_SPECIFIED'
+     INNER JOIN concept_name htcTieBreakerPositive on htcTieBreakerObs.value_coded = htcTieBreakerPositive.concept_id and htcTieBreakerPositive.name ='Positive' and htcTieBreakerPositive.concept_name_type = 'FULLY_SPECIFIED') htcTieBreaker on htcTieBreaker.obs_id = obs.obs_id
+where ((pncVisitTime.obs_id is not null) and (resultIfTested.obs_id is not null or htcConfirmatory.obs_id is not null or htcInitial.obs_id is not null or htcTieBreaker.obs_id is not null))  
+      AND DATE(obs.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
