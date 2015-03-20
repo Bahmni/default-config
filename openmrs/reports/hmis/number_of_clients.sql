1a528c886f2368fe9bc7d495606b260a271aa221
Preethi <preethis@thoughtworks.com>
2015-03-20 11:19:09 +0530

    Preethi , Sudhakar | #CustomSql | Updated the labels to have spaces instead of underscores

--- openmrs/reports/hmis/number_of_clients.sql
+++ openmrs/reports/hmis/number_of_clients.sql
@@ -1,8 +1,8 @@
-SELECT client_visits.age_group AS Age_Group,
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'F', 1, 0),0))) AS New_Clients_Female,
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'M', 1, 0),0))) AS New_Clients_Male,
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) as Total_Clients_Female,
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) as Total_Clients_Male
+SELECT client_visits.age_group AS 'Age Group',
+       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'F', 1, 0),0))) AS 'New Clients Female',
+       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'M', 1, 0),0))) AS 'New Clients Male',
+       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) as 'Total Clients Female',
+       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) as 'Total Clients Male'
 FROM
   (SELECT DISTINCT patient.patient_id AS patient_id,
                    observed_age_group.name AS age_group,
