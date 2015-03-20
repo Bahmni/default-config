7c4b6e833c4a56a81419d4195cf1516cfd175dfb
Hemanth S.R <hemanthfeb25@gmail.com>
2015-03-20 12:34:58 +0530

    Hemanth | filtering voided obs and patients, casting datetime to date.

--- openmrs/reports/hmis/HIV_new_patients.sql
+++ openmrs/reports/hmis/HIV_new_patients.sql
@@ -21,45 +21,45 @@ SELECT 'New patients enrolled in HIV care' AS 'Anteretroviral Treatment : Status
        ifnull(sum(if(person_id IN
                        (SELECT person_id
                         FROM person_ids
-                        WHERE concept_full_name = 'ART, Risk Group'
+                        WHERE concept_full_name = 'HIVTC, Risk Group'
                           AND value_concept_full_name = 'Sex Worker'
                           AND gender='F'
-                          AND obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+                          AND DATE(obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
                         GROUP BY person_id),1,0)),0) AS 'FSW',
        ifnull(sum(if(person_id IN
                        (SELECT person_id
                         FROM person_ids
-                        WHERE concept_full_name = 'ART, Risk Group'
+                        WHERE concept_full_name = 'HIVTC, Risk Group'
                           AND value_concept_full_name = 'People Who Inject Drugs'
-                          AND obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+                          AND DATE(obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
                         GROUP BY person_id),1,0)),0) AS 'IDU',
        ifnull(sum(if(person_id IN
                        (SELECT person_id
                         FROM person_ids
-                        WHERE concept_full_name = 'ART, Risk Group'
+                        WHERE concept_full_name = 'HIVTC, Risk Group'
                           AND value_concept_full_name = 'MSM and Transgenders'
-                          AND obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+                          AND DATE(obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
                         GROUP BY person_id),1,0)),0) AS 'MSM',
        ifnull(sum(if(person_id IN
                        (SELECT person_id
                         FROM person_ids
-                        WHERE concept_full_name = 'ART, Risk Group'
+                        WHERE concept_full_name = 'HIVTC, Risk Group'
                           AND value_concept_full_name = 'Migrant'
-                          AND obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+                          AND DATE(obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
                         GROUP BY person_id),1,0)),0) AS 'Migrant',
        ifnull(sum(if(person_id IN
                        (SELECT person_id
                         FROM person_ids
-                        WHERE concept_full_name = 'ART, Risk Group'
+                        WHERE concept_full_name = 'HIVTC, Risk Group'
                           AND value_concept_full_name NOT IN ('Migrant', 'MSM and Transgenders', 'Sex Worker', 'People Who Inject Drugs')
-                          AND obs_datetime BETWEEN '#startDate#' AND '#endDate#'
+                          AND DATE(obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
                         GROUP BY person_id),1,0)),0) AS 'Others'
 FROM
   (SELECT o.person_id,
-          datediff(o.obs_datetime,p.birthdate)/365 AS 'age',
+          datediff(DATE(o.obs_datetime),DATE(p.birthdate))/365 AS 'age',
           p.gender
    FROM obs_view o
-   INNER JOIN person p ON o.person_id = p.person_id
-   WHERE o.concept_full_name='HIVTC, HIV care enrolled date'
-     AND o.value_datetime BETWEEN '#startDate#' AND '#endDate#'
+   INNER JOIN person p ON o.person_id = p.person_id AND p.voided = 0
+   WHERE o.concept_full_name='HIVTC, HIV care enrolled date' AND o.voided = 0
+     AND DATE(o.obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
    GROUP BY o.person_id) AS t2
