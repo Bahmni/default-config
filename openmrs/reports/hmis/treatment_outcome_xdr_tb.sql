727b5c5a983fc75ec8b91c08e0ccc9dc8b7e21c5
Preethi <preethis@thoughtworks.com>
2015-03-20 12:04:29 +0530

    Preethi, Sudhakar | #CustomSql | Ignoring revised diagnoses in xdr tb treatment outcome report

--- openmrs/reports/hmis/treatment_outcome_xdr_tb.sql
+++ openmrs/reports/hmis/treatment_outcome_xdr_tb.sql
@@ -17,10 +17,12 @@ FROM
      JOIN concept_view AS xdr_tb_concept ON xdr_tb_concept.concept_id = xdr_tb_obs.value_coded AND xdr_tb_concept.concept_full_name = 'Extremely Drug Resistant Tuberculosis'
      JOIN obs AS certainty_obs ON certainty_obs.person_id = xdr_tb_obs.person_id AND certainty_obs.obs_group_id = xdr_tb_obs.obs_group_id AND certainty_obs.voided IS FALSE
      JOIN concept_name AS certainty_concept ON certainty_concept.concept_id = certainty_obs.value_coded AND certainty_concept.concept_name_type = 'FULLY_SPECIFIED' AND certainty_concept.name = 'Confirmed'
+     JOIN concept_name revised_concept ON revised_concept.name = 'Bahmni Diagnosis Revised' AND revised_concept.concept_name_type = 'FULLY_SPECIFIED'
+     LEFT JOIN obs as revised_obs on revised_concept.concept_id = revised_obs.concept_id and revised_obs.voided is false and xdr_tb_obs.obs_group_id = revised_obs.obs_group_id
      JOIN concept_name ruled_out_concept ON ruled_out_concept.name = 'Ruled Out Diagnosis' AND ruled_out_concept.concept_name_type = 'FULLY_SPECIFIED'
      LEFT JOIN obs ruled_out ON ruled_out.value_coded = ruled_out_concept.concept_id
                                 AND ruled_out.obs_group_id = xdr_tb_obs.obs_group_id AND ruled_out.voided IS FALSE
-   WHERE ruled_out.value_coded IS NULL
+   WHERE ruled_out.value_coded IS NULL and revised_obs.value_coded = (select property_value from global_property where property = 'concept.false')
   ) AS outcome
     ON outcome_concept_answer.answer_concept = outcome.value_coded AND DATE(outcome.obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
   LEFT JOIN person p ON outcome.person_id = p.person_id AND p.voided IS FALSE
