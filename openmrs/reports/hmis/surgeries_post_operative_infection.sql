8dc73e123a04b3fb003e02265e81d63e7cf3992f
Mujir <mujir@thoughtworks.com>
2015-03-23 15:32:39 +0530

    Mujir | #000 | fixed post operative infection sql to use bahmni diagnosis view. Fixes all diagnosis related issues for revised, voided, ruled out obs

--- openmrs/reports/hmis/surgeries_post_operative_infection.sql
+++ openmrs/reports/hmis/surgeries_post_operative_infection.sql
@@ -1,20 +1,16 @@
-select rag.name,cn.name as SurgeryType,coded_concept_name.name as Diagnosis,va.value_reference,
+select rag.name,cn.name as SurgeryType, diagnosis.name as Diagnosis, va.value_reference,
   SUM(IF(p.gender = 'F', 1, 0)) AS female,
   SUM(IF(p.gender = 'M', 1, 0)) AS male
-from visit v
-  INNER JOIN encounter diagnosis_encounter on v.visit_id = diagnosis_encounter.visit_id
-  INNER JOIN obs diagnosis_obs on diagnosis_encounter.encounter_id = diagnosis_obs.encounter_id
-  INNER JOIN concept_name diagnosis_concept_name on diagnosis_concept_name.concept_id = diagnosis_obs.concept_id
-  INNER JOIN concept_name coded_concept_name on coded_concept_name.concept_id = diagnosis_obs.value_coded
-  INNER JOIN encounter e on v.visit_id = e.visit_id
-  INNER JOIN obs obs on e.encounter_id = obs.encounter_id
-  INNER JOIN concept_name cn on cn.concept_id = obs.concept_id
-  INNER JOIN person p on p.person_id = obs.person_id
-  INNER JOIN visit_attribute va on va.visit_id = v.visit_id
-  INNER JOIN reporting_age_group rag ON rag.report_group_name = 'All Ages' AND
-                                        obs.obs_datetime BETWEEN (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY))
-                                        AND (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
-where cn.name in ('Operative Notes, Procedure','Procedure Notes, Procedure') 
-      and diagnosis_concept_name.name = 'Coded Diagnosis' and diagnosis_concept_name.concept_name_type = 'FULLY_SPECIFIED'
-      and coded_concept_name.name in ('Infection of obstetric surgical wound','Infection and inflammatory reaction due to other internal orthopedic prosthetic devices, implants and grafts, initial encounter','Infection following a procedure') and coded_concept_name.concept_name_type = 'FULLY_SPECIFIED'
-GROUP BY rag.name,cn.name,coded_concept_name.name,va.value_reference;
+from visit diagnosis_visit
+  INNER JOIN confirmed_diagnosis_view diagnosis on diagnosis.visit_id = diagnosis_visit.visit_id
+        and diagnosis.name in ('Infection of obstetric surgical wound','Infection and inflammatory reaction due to other internal orthopedic prosthetic devices, implants and grafts, initial encounter','Infection following a procedure')
+        AND DATE(diagnosis_visit.date_stopped) BETWEEN '#startDate#' AND '#endDate#'
+  INNER JOIN encounter diagnosis_encounter on diagnosis_encounter.visit_id = diagnosis.visit_id
+  INNER JOIN obs all_obs on diagnosis_encounter.encounter_id = all_obs.encounter_id
+  INNER JOIN concept_name cn on cn.concept_id = all_obs.concept_id and cn.name in ('Operative Notes, Procedure','Procedure Notes, Procedure')
+  INNER JOIN person p on p.person_id = all_obs.person_id
+  INNER JOIN visit_attribute va on va.visit_id = diagnosis_visit.visit_id
+  INNER JOIN reporting_age_group rag ON rag.report_group_name = 'All Ages'
+        AND all_obs.obs_datetime BETWEEN (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY))
+        AND (DATE_ADD(DATE_ADD(p.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
+GROUP BY rag.name, cn.name, diagnosis.name, va.value_reference;
