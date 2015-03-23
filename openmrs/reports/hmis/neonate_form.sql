a598c04a23106c3e97b2beb77eb00464777e361f
Hemanth S.R <hemanthfeb25@gmail.com>
2015-03-23 10:43:20 +0530

    Hemanth | filtering voided obs and joining concept_view than concept_name

--- openmrs/reports/hmis/neonate_form.sql
+++ openmrs/reports/hmis/neonate_form.sql
@@ -1,24 +1,33 @@
-select
-    delivery_outcome as "Count of Delivery Outcome / Gestation Period",
-    SUM(22_27) as "22 - 27",
-    SUM(28_36) as "28 - 36",
-    SUM(37_41) as "37 - 41",
-    SUM(above41) as ">= 42"
-from
-    (select
-        IF(outcome_value.name like 'Single%', "Primi", IF(outcome_value.name like 'Twins%', "Multi", "Grand Multi")) as delivery_outcome,
-        IF(outcome_value.name like 'Single%', 1, IF(outcome_value.name like 'Twins%', 2, 3)) as sort_order,
-        SUM(if(gestation_period_obs.value_numeric between 22 and 27, 1, 0)) as 22_27,
-        SUM(if(gestation_period_obs.value_numeric between 28 and 36, 1, 0)) as 28_36,
-        SUM(if(gestation_period_obs.value_numeric between 37 and 41, 1, 0)) as 37_41,
-        SUM(if(gestation_period_obs.value_numeric >= 42, 1, 0)) as above41
-    from obs outcome_of_delivery_obs
-        left outer join obs gestation_period_obs on outcome_of_delivery_obs.person_id = gestation_period_obs.person_id and  gestation_period_obs.concept_id = (select concept_id from concept_name where name = 'Delivery Note, Gestation period' and concept_name_type = 'FULLY_SPECIFIED') and gestation_period_obs.voided = false
-        inner join concept_name outcome_value on outcome_value.concept_id = outcome_of_delivery_obs.value_coded and outcome_value.concept_name_type = 'FULLY_SPECIFIED'
-    where
-        outcome_of_delivery_obs.concept_id = (select concept_id from concept_name where name = "Delivery Note, Outcome of Delivery" and concept_name_type="FULLY_SPECIFIED")
-        and CAST(outcome_of_delivery_obs.obs_datetime as DATE) between "#startDate#" and "#endDate#"
-        and outcome_of_delivery_obs.voided = false
-    group by outcome_value.name) simpler_form
-group by delivery_outcome
-order by sort_order;
+SELECT
+  delivery_outcome AS 'Count of Delivery Outcome / Gestation Period',
+  SUM(22_27)       AS '22 - 27',
+  SUM(28_36)       AS '28 - 36',
+  SUM(37_41)       AS '37 - 41',
+  SUM(above41)     AS '>= 42'
+FROM
+  (SELECT
+     IF(outcome_value.concept_full_name LIKE 'Single%', 'Primi', 
+     IF(outcome_value.concept_full_name LIKE 'Twins%', 'Multi', 'Grand Multi'))                     AS delivery_outcome,
+     IF(outcome_value.concept_full_name LIKE 'Single%', 1, IF(outcome_value.concept_full_name LIKE 'Twins%', 2, 3)) AS sort_order,
+     SUM(if(gestation_period_obs.value_numeric BETWEEN 22 AND 27, 1, 0))                  AS 22_27,
+     SUM(if(gestation_period_obs.value_numeric BETWEEN 28 AND 36, 1, 0))                  AS 28_36,
+     SUM(if(gestation_period_obs.value_numeric BETWEEN 37 AND 41, 1, 0))                  AS 37_41,
+     SUM(if(gestation_period_obs.value_numeric >= 42, 1, 0))                              AS above41
+   FROM obs outcome_of_delivery_obs
+     LEFT OUTER JOIN obs gestation_period_obs
+       ON outcome_of_delivery_obs.person_id = gestation_period_obs.person_id
+          AND gestation_period_obs.concept_id = (SELECT concept_id
+                                                 FROM concept_view
+                                                 WHERE concept_full_name = 'Delivery Note, Gestation period')
+          AND gestation_period_obs.voided = FALSE
+     INNER JOIN concept_view outcome_value
+       ON outcome_value.concept_id = outcome_of_delivery_obs.value_coded
+   WHERE
+     outcome_of_delivery_obs.concept_id = (SELECT concept_id
+                                           FROM concept_view
+                                           WHERE concept_full_name = 'Delivery Note, Outcome of Delivery')
+     AND DATE(outcome_of_delivery_obs.obs_datetime) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+     AND outcome_of_delivery_obs.voided = FALSE
+   GROUP BY outcome_value.concept_full_name) simpler_form
+GROUP BY delivery_outcome
+ORDER BY sort_order;
