5cf53c2ecca0e36cb89c89b510f3ab279fd12a75
Sudhakar <sudhakar82@gmail.com>
2015-07-03 12:04:25 +0530

    Abishek, Sudhakar | #2508 | Moved the growth chart to trends tab in dashboard

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -5,7 +5,7 @@ source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 IMPLEMENTATION_NAME=default
 APP_NAME=${1:-openmrs}
 MIGRATIONS_DIR="/packages/build/${IMPLEMENTATION_NAME}_config/${APP_NAME}/migrations"
-SRC_MIGRATIONS_DIR="/Project/${IMPLEMENTATION_NAME}-config/${APP_NAME}/migrations"
+SRC_MIGRATIONS_DIR="/bahmni/${IMPLEMENTATION_NAME}-config/${APP_NAME}/migrations"
 
 run_in_vagrant -c "sudo rm -rf ${MIGRATIONS_DIR}/*"
 run_in_vagrant -c "sudo mkdir -p ${MIGRATIONS_DIR}"
