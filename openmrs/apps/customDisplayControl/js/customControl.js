47158b3598edefab859f02385afee4fc0fd2e069
hemanths <hemanths@INhemanths.local>
2015-07-30 11:41:08 +0530

    Charles, Hemanth | custom display control

---
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('bahmni.common.displaycontrol.custom')
+    .directive('birthCertificate', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
+            var link = function ($scope) {
+                console.log("inside birth certificate");
+                var conceptNames = ["HEIGHT"];
+                $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/birthCertificate.html";
+                spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
+                    $scope.observations = response.data;
+                }));
+            };
+
+            return {
+                restrict: 'E',
+                template: '<ng-include src="contentUrl"/>',
+                link: link
+            }
+    }]).directive('deathCertificate', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
+        var link = function ($scope) {
+            var conceptNames = ["WEIGHT"];
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/deathCertificate.html";
+            spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
+                $scope.observations = response.data;
+            }));
+        };
+
+        return {
+            restrict: 'E',
+            link: link,
+            template: '<ng-include src="contentUrl"/>'
+        }
+    }]);
