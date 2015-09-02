601390c7d48da5c2522227073ef53c5ba6835660
Sudhakar <sudhakar82@gmail.com>
2015-09-02 15:30:47 +0530

    Disabling the form conditions

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,28 +1,2 @@
-Bahmni.ConceptSet.FormConditions = {
-    'Diastolic Data' : function (value, formFieldValues) {
-        var systolic = formFieldValues['Systolic'];
-        var diastolic = formFieldValues['Diastolic'];
-        if (systolic || diastolic) {
-            return {
-                enable: ["Posture"]
-            }
-        } else {
-            return {
-                disable: ["Posture"]
-            }
-        }
-    },
-    'Systolic Data' : function (value, formFieldValues) {
-        var systolic = formFieldValues['Systolic'];
-        var diastolic = formFieldValues['Diastolic'];
-        if (systolic || diastolic) {
-            return {
-                enable: ["Posture"]
-            }
-        } else {
-            return {
-                disable: ["Posture"]
-            }
-        }
-    }
+Bahmni.ConceptSet.FormConditions.rules = {
 };
