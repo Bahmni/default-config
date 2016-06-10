1607f910dd82fd98bc712e3157c44e55298cd808
sadiag <sadia16.ghazal@gmail.com>
2016-06-10 15:36:40 +0530

    sadia | added custom display control for Birth certificate

---
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -0,0 +1,30 @@
+'use strict';
+angular.module('bahmni.common.displaycontrol.custom')
+    .directive('birthCertificates', ['$q','observationsService','appService', 'spinner','$sce', function ($q,observationsService, appService, spinner, $sce)
+    {
+        var link = function ($scope)
+        {
+
+            var conceptNames = ["Birth Note Set"];
+            spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
+                    $scope.observations = response.data[0];
+
+
+                }));
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/birthCertificatepo.html";
+            $scope.curDate=new Date();
+
+
+        };
+        var controller = function($scope){
+                $scope.htmlLabel = function(label){
+                        return $sce.trustAsHtml(label)
+                }
+        }
+        return {
+            restrict: 'E',
+            link: link,
+            controller : controller,
+            template: '<ng-include src="contentUrl"/>'
+        }
+    }]);
