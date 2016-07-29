5538b0d7eeae8c571d55803420825e1c6e865411
sadiag <sadia16.ghazal@gmail.com>
2016-07-29 16:15:46 +0530

    sadia | added birth certificate changes

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -1,3 +1,4 @@
+
 'use strict';
 angular.module('bahmni.common.displaycontrol.custom')
     .directive('birthCertificates', ['$q','observationsService','appService', 'spinner','$sce', function ($q,observationsService, appService, spinner, $sce)
@@ -8,6 +9,49 @@ angular.module('bahmni.common.displaycontrol.custom')
             var conceptNames = ["Delivery Note, Liveborn infant details"];
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "previous", undefined, $scope.visitUuid, undefined).then(function (response) {
                     $scope.observations = response.data;
+                                            if ($scope.observations==undefined)
+                                            { 
+                                                console.log("template not filled");
+                                                return;
+                                            }
+
+	 	     $scope.birthCertificates = [];
+
+
+
+                                        function createForm(obs, birthCertificate) {
+                                               if (obs.groupMembers.length == 0){
+                                                  if (birthCertificate[obs.conceptNameToDisplay] == undefined){
+                                                     birthCertificate[""+obs.conceptNameToDisplay] = obs.valueAsString;
+                                                  }
+                                                  else{
+                                                     birthCertificate[""+obs.conceptNameToDisplay] = birthCertificate[""+obs.conceptNameToDisplay] + ' ' + obs.valueAsString;
+                                                  }
+
+                                                  if(obs.comment != null){
+                                                    birthCertificate[""+obs.conceptNameToDisplay] = birthCertificate[""+obs.conceptNameToDisplay] + ' ' + obs.comment;
+                                                  }
+                                               }
+                                               else{
+                                                  for(var i = 0; i < obs.groupMembers.length; i++) {
+                                                       createForm(obs.groupMembers[i], birthCertificate);
+                                                  }
+
+
+                                               }
+
+                            }
+
+
+
+                            for(var j=0;j<response.data.length;j++){
+                                var birthCertificate = [];
+                                createForm(response.data[j], birthCertificate);
+                                $scope.birthCertificates.push(birthCertificate);
+                            }
+
+
+
 
 
                 }));
