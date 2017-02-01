4fb3a185099e810ad32ac67a2884bb2f5b436706
Yash <ydave@thoughtworks.com>
2017-02-01 12:41:51 +0530

    Yash,Gautam|Merged Prod Server changes to master

--- openmrs/obscalculator/BahmniObsValueCalculator.groovy
+++ openmrs/obscalculator/BahmniObsValueCalculator.groovy
@@ -177,14 +177,13 @@ public class BahmniObsValueCalculator implements ObsValueCalculator {
                 " and obs.voided = false" +
                 " and obs.obsDatetime <= :till" +
                 excludeObsClause +
-                " order by obs.obsDatetime desc ");
+                " order by obs.obsDatetime desc limit 1");
         queryToGetObservations.setString("patientUuid", patientUuid);
         queryToGetObservations.setParameterList("conceptName", conceptName);
         queryToGetObservations.setParameter("till", tillDate);
         if (excludedObsIsSaved) {
             queryToGetObservations.setString("excludeObsUuid", excludeObs.uuid)
         }
-        queryToGetObservations.setMaxResults(1);
         List<Obs> observations = queryToGetObservations.list();
         if (observations.size() > 0) {
             return observations.get(0).getValueNumeric();
