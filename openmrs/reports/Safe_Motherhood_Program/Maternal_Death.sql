7c8b80e81f10ba97be1af87bbbdbac917ba432e5
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-04-20 18:35:37 +0530

    Sidtharthan | Simplify, Ensure default output for, Safe_Motherhood_Program/Maternal_Death

--- openmrs/reports/Safe_Motherhood_Program/Maternal_Death.sql
+++ openmrs/reports/Safe_Motherhood_Program/Maternal_Death.sql
@@ -1,16 +1,8 @@
-SELECT 'Maternal Death' AS 'Header',
-       SUM(IF(Maternal_Death_Info.NAME LIKE '%AntePartum%', Maternal_Death_Info.COUNT, 0)) AS 'Ante-partum',
-       SUM(IF(Maternal_Death_Info.NAME LIKE '%IntraPartum%', Maternal_Death_Info.COUNT, 0)) AS 'Intra-partum',
-       SUM(IF(Maternal_Death_Info.NAME LIKE '%PostPartum%', Maternal_Death_Info.COUNT, 0)) AS 'Post-partum'
-FROM
-  (SELECT t2.name AS NAME, COUNT(*) AS COUNT FROM obs t1
-    INNER JOIN concept_name t2 ON t1.value_coded = t2.concept_id
-                                  AND t2.voided = 0 AND t2.concept_name_type = 'FULLY_SPECIFIED'
-    INNER JOIN encounter t3 ON t1.encounter_id = t3.encounter_id
-    INNER JOIN visit t4 ON t3.visit_id = t4.visit_id
-    INNER JOIN concept_name t5 ON t1.concept_id = t5.concept_id AND t5.voided = 0
-                                  AND t5.concept_name_type = 'FULLY_SPECIFIED'
-  WHERE t5.name = 'Death Note, Maternal Death'
-        AND t1.voided = 0  AND
-        (t1.obs_datetime >='#startDate#' AND t1.obs_datetime <= '#endDate#')
-  GROUP BY t2.name) AS Maternal_Death_Info;
+SELECT
+  'Maternal Death'                                                  AS 'Header',
+  COALESCE(SUM(IF(answer_full_name LIKE '%AntePartum%', 1, 0)), 0)  AS 'Ante-partum',
+  COALESCE(SUM(IF(answer_full_name LIKE '%IntraPartum%', 1, 0)), 0) AS 'Intra-partum',
+  COALESCE(SUM(IF(answer_full_name LIKE '%PostPartum%', 1, 0)), 0)  AS 'Post-partum'
+FROM nonVoidedQuestionAnswerObs
+WHERE question_full_name = 'Death Note, Maternal Death'
+      AND obs_datetime >= '#startDate#' AND obs_datetime <= '#endDate#';
