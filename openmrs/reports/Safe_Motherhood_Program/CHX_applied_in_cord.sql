417849f421b8af5e276be36c743d17964bd74996
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-04-20 18:07:51 +0530

    Sidtharthan | Simplify, Ensure default output for, Safe_Motherhood_Program/CHX_applied_in_cord

--- openmrs/reports/Safe_Motherhood_Program/CHX_applied_in_cord.sql
+++ openmrs/reports/Safe_Motherhood_Program/CHX_applied_in_cord.sql
@@ -1,12 +1,7 @@
-SELECT COUNT(*) AS 'CHX applied in cord' FROM obs t1
-  INNER JOIN concept_name t2 ON t1.value_coded = t2.concept_id
-                                AND t2.voided = 0 AND t2.concept_name_type = 'FULLY_SPECIFIED'
-  INNER JOIN encounter t3 ON t1.encounter_id = t3.encounter_id
-  INNER JOIN visit t4 ON t3.visit_id = t4.visit_id
-  INNER JOIN concept_name t5 ON t1.concept_id = t5.concept_id AND t5.voided = 0
-                                AND t5.concept_name_type = 'FULLY_SPECIFIED'
-WHERE t5.name LIKE 'Delivery Note, Chlorhexidine%'
-      AND t1.voided = 0 AND t1.value_coded = 1 AND
-      (t1.obs_datetime >='#startDate#' AND t1.obs_datetime <= '#endDate#')
-GROUP BY t2.name
-ORDER BY t2.name;
+#No need to union with default values as the query itself returns 0
+####################################################################
+SELECT COUNT(*) AS 'CHX applied in cord'
+FROM nonVoidedQuestionAnswerObs
+WHERE question_full_name = 'Delivery Note, Chlorhexidine applied on cord'
+      AND answer_full_name = 'True'
+      AND obs_datetime >= '#startDate#' AND obs_datetime <= '#endDate#';
