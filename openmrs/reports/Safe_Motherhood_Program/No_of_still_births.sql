c702f251ed4c95e5a35ef4978d9014bb95667f54
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-04-20 19:03:26 +0530

    Sidtharthan | Simplify, Ensure default output for, Safe_Motherhood_Program/No_of_still_births

--- openmrs/reports/Safe_Motherhood_Program/No_of_still_births.sql
+++ openmrs/reports/Safe_Motherhood_Program/No_of_still_births.sql
@@ -1,12 +1,16 @@
-SELECT t2.name AS StillBirthType , COUNT(*) AS 'No of still births' FROM obs t1
-INNER JOIN concept_name t2 ON t1.value_coded = t2.concept_id
-AND t2.voided = 0 AND t2.concept_name_type = 'FULLY_SPECIFIED' AND t2.Name <> 'Stillbirth'
-INNER JOIN encounter t3 ON t1.encounter_id = t3.encounter_id
-INNER JOIN visit t4 ON t3.visit_id = t4.visit_id
-INNER JOIN concept_name t5 ON t1.concept_id = t5.concept_id AND t5.voided = 0
-AND t5.concept_name_type = 'FULLY_SPECIFIED'
-WHERE t5.name IN ('Delivery Note, StillBirth Type')
-AND t1.voided = 0 AND
-(t1.obs_datetime >='#startDate#' AND t1.obs_datetime <= '#endDate#')
-GROUP BY t2.name
-ORDER BY t2.name;
+SELECT final.StillBirthType AS 'Still birth type',
+  sum(final.count) AS 'No of still births'
+FROM
+(SELECT
+ answer_full_name AS StillBirthType,
+ COUNT(*)         AS count
+FROM nonVoidedQuestionAnswerObs
+WHERE question_full_name = 'Delivery Note, StillBirth Type'
+     AND obs_datetime >= '#startDate#' AND obs_datetime <= '#endDate#'
+GROUP BY answer_full_name
+UNION ALL SELECT 'Fresh stillbirth',0
+UNION ALL SELECT 'Macerated stillbirth',0
+UNION ALL SELECT 'Not Applicable',0
+) final
+GROUP BY final.StillBirthType
+ORDER BY final.StillBirthType;
