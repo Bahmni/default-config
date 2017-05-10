62428898d2f7be71d787690fd17f6889b8d83291
Sidtharthan_A_N <sidthunagarajan@gmail.com>
2017-05-10 14:18:56 +0530

    Sidtharthan | Renamed reports column header in Safe_Motherhood_Program/Blood_transfusion.

--- openmrs/reports/Safe_Motherhood_Program/Blood_transfusion.sql
+++ openmrs/reports/Safe_Motherhood_Program/Blood_transfusion.sql
@@ -4,8 +4,8 @@ SELECT
 FROM
 -- ----------------------------------------------
 (SELECT
-   'No'     AS Blood,
-   COUNT(*) AS BloodTransfusion
+   'No of Females' AS Blood,
+   COUNT(*)        AS BloodTransfusion
  FROM obs t1
    INNER JOIN concept_name t2 ON t1.value_coded = t2.concept_id
                                  AND t2.voided = 0 AND t2.concept_name_type = 'FULLY_SPECIFIED' AND t2.Name <> 'Stillbirth'
@@ -31,7 +31,7 @@ SELECT
        (t1.obs_datetime >= '#startDate#' AND t1.obs_datetime <= '#endDate#')
  GROUP BY t5.name
 -- ----------------------------------------------
-UNION ALL SELECT 'No',0
+UNION ALL SELECT 'No of Females',0
 UNION ALL SELECT 'Unit',0) final
 GROUP BY final.Blood
 ORDER BY final.Blood;
