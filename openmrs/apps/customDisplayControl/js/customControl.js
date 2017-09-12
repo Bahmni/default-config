1d8067f4e7f6cbbe2c1d6b55a1deaeff7224c6b7
shireesha <bongaralashireesha@gmail.com>
2017-09-12 11:51:31 +0530

    Maharjun, Shireesha | #3756 | Custom display control for patient appointments

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -149,4 +149,51 @@ angular.module('bahmni.common.displaycontrol.custom')
         },
         template: '<ng-include src="contentUrl"/>'
     }
+}]).directive('patientAppointmentsDashboard', ['$http', '$q', '$window','appService', function ($http, $q, $window, appService) {
+    var link = function ($scope) {
+        $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/patientAppointmentsDashboard.html";
+        var getUpcomingAppointments = function () {
+            var params = {
+                q: "bahmni.sqlGet.upComingAppointments",
+                v: "full",
+                patientUuid: $scope.patient.uuid
+            };
+            return $http.get('/openmrs/ws/rest/v1/bahmnicore/sql', {
+                method: "GET",
+                params: params,
+                withCredentials: true
+            });
+        };
+        var getPastAppointments = function () {
+            var params = {
+                q: "bahmni.sqlGet.pastAppointments",
+                v: "full",
+                patientUuid: $scope.patient.uuid
+            };
+            return $http.get('/openmrs/ws/rest/v1/bahmnicore/sql', {
+                method: "GET",
+                params: params,
+                withCredentials: true
+            });
+        };
+        $q.all([getUpcomingAppointments(), getPastAppointments()]).then(function (response) {
+            $scope.upcomingAppointments = response[0].data;
+            $scope.upcomingAppointmentsHeadings = _.keys($scope.upcomingAppointments[0]);
+            $scope.pastAppointments = response[1].data;
+            $scope.pastAppointmentsHeadings = _.keys($scope.pastAppointments[0]);
+        });
+
+        $scope.goToListView = function () {
+            $window.open('/bahmni/appointments/#/home/manage/appointments/list');
+        };
+    };
+    return {
+        restrict: 'E',
+        link: link,
+        scope: {
+            patient: "=",
+            section: "="
+        },
+        template: '<ng-include src="contentUrl"/>'
+    };
 }]);
---
+++ openmrs/migrations/patientUpcomingAppointments.sql
@@ -0,0 +1,26 @@
+DELETE FROM global_property WHERE property = 'bahmni.sqlGet.upComingAppointments';
+SELECT uuid() INTO @uuid;
+
+INSERT INTO global_property (property, property_value, description, uuid)
+ VALUES ('bahmni.sqlGet.upComingAppointments',
+"SELECT
+  app_service.name                                                                                AS `DASHBOARD_APPOINTMENTS_SERVICE_KEY`,
+  app_service_type.name                                                                           AS `DASHBOARD_APPOINTMENTS_SERVICE_TYPE_KEY`,
+  DATE_FORMAT(start_date_time, \"%d/%m/%Y\")                                                        AS `DASHBOARD_APPOINTMENTS_DATE_KEY`,
+  CONCAT(DATE_FORMAT(start_date_time, \"%l:%i %p\"), \" - \", DATE_FORMAT(end_date_time, \"%l:%i %p\")) AS `DASHBOARD_APPOINTMENTS_SLOT_KEY`,
+  prov.name                                                                                       AS `DASHBOARD_APPOINTMENTS_PROVIDER_KEY`,
+  pa.status                                                                                       AS `DASHBOARD_APPOINTMENTS_STATUS_KEY`
+FROM
+  patient_appointment pa
+  JOIN person p ON p.person_id = pa.patient_id AND pa.voided IS FALSE
+  JOIN appointment_service app_service
+    ON app_service.appointment_service_id = pa.appointment_service_id AND app_service.voided IS FALSE
+  LEFT JOIN provider prov ON prov.provider_id = pa.provider_id
+  LEFT JOIN appointment_service_type app_service_type
+    ON app_service_type.appointment_service_type_id = pa.appointment_service_type_id
+       AND app_service_type.voided IS FALSE
+WHERE p.uuid = ${patientUuid} AND
+      start_date_time >= CURDATE() AND
+      (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL)
+ORDER BY start_date_time ASC;"
+, 'Upcoming appointments for patient', @uuid);
