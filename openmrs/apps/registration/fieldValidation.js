17cf692c066a3e224cbcf6467f2496545eb27b91
RafaNetow <rafanetow@gmail.com>
2017-10-20 10:25:20 -0600

    Revert "Report json file"

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -5,34 +5,16 @@ Bahmni.Registration.customValidator = {
         },
         errorMessage: "REGISTRATION_AGE_ERROR_KEY"
     },
-    "cellphone": {
+    "Telephone Number": {
         method: function (name, value, personAttributeDetails) {
-            return value.match(/\(?([0-9]{3})\)?([ .-]?)([0-9]{4})-([0-9]{4})/);
+            return value && value.length> 6;
         },
-        errorMessage: "REGISTRATION_CELLPHONE_TEXT_ERROR_KEY"
+        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
     },
-    "telephoneHouse": {
+    "caste": {
         method: function (name, value, personAttributeDetails) {
-            return value.match(/[0-9]{4}-[0-9]{4}/);
+            return value.match(/^\w+$/);
         },
-        errorMessage: "REGISTRATION_TELEPHONE_TEXT_ERROR_KEY"
-    },
-    "telephoneWork": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/[0-9]{4}-[0-9]{4}/);
-        },
-        errorMessage: "REGISTRATION_TELEPHONE_TEXT_ERROR_KEY"
-    },
-    "emailPersonal": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        },
-        errorMessage: "REGISTRATION_EMAIL_TEXT_ERROR_KEY"
-    },
-    "emailWork": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        },
-        errorMessage: "REGISTRATION_EMAIL_TEXT_ERROR_KEY"
+        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
     }
 };
