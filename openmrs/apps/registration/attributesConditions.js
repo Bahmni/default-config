25f900f22e352e667196d68b832d31ee295f18bc
Cristian Echeverria <cristian.echeverri4@gmail.com>
2017-10-21 11:19:26 -0600

    Se agregó validación para el Numero de Identificación del Paciente ya sea Tarjeta de Identidad, Residencia o Pasaporte

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -1,9 +1,48 @@
 Bahmni.Registration.AttributesConditions.rules = {
     'patientType': function(patient) {
         return showOrHideServiceInfoSection(patient);
+    },
+    'identifierDocument': function(patient) {
+        if (patient['identifierDocument']) {
+            switch (patient['identifierDocument'].conceptUuid) {
+                case "c74c7f3b-954f-41e1-ba26-b595906020b5":
+                    Bahmni.Registration.customValidator = {
+                        "primaryIdentifier.registrationNumber": {
+                            method: function (name, value) {
+                                return value.match(/[0-9]{13}/);
+                            },
+                            errorMessage: "REGISTRATION_ID_TEXT_ERROR_KEY"
+                        }
+                    };
+                    break;
+                
+                case "dafa2194-f468-4b6e-9b3f-97fbb181de55":
+                    Bahmni.Registration.customValidator = {
+                        "primaryIdentifier.registrationNumber": {
+                            method: function (name, value) {
+                                return value.match(/[0-9]{15}/);
+                            },
+                            errorMessage: "REGISTRATION_RESIDENCE_TEXT_ERROR_KEY"
+                        }
+                    };
+                    break;
+            
+                default:
+                    Bahmni.Registration.customValidator = {
+                        "primaryIdentifier.registrationNumber": {
+                            method: function (name, value) {
+                                return value.match(/[a-zA-Z\-0-9]+/);
+                            },
+                            errorMessage: "REGISTRATION_PASSPORT_TEXT_ERROR_KEY"
+                        }
+                    };
+                    break;
+            }
+        }
+        
+        return true;
     }
 };
-
 
 var showOrHideServiceInfoSection = function (patient) {
     var returnValues = {
