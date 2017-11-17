0b36daf4abce137e6d1303c9f527e7bed876f82a
Luis Carlos Isaula <luisca_elfather@hotmail.com>
2017-11-17 12:24:13 -0600

    chaging .sh files for plain text in deploy.sh

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -10,8 +10,10 @@ if [[ "$BUILDKITE_BRANCH" == "staging"  ]]; then
   sudo scp -i $AWS_PEM_KEY_STAGING -r ./openelis $AWS_USER_STAGING@$AWS_IP_STAGING:$AWS_DESTINATION_PATH_STAGING | tee -a bk-pipeline.log
   sudo scp -i $AWS_PEM_KEY_STAGING -r ./openmrs $AWS_USER_STAGING@$AWS_IP_STAGING:$AWS_DESTINATION_PATH_STAGING | tee -a bk-pipeline.log
   DUMP_NAME=$BUILDKITE_BRANCH"-"$BUILDKITE_BUILD_NUMBER".sql"
-  ssh -i $AWS_PEM_KEY_DEV $AWS_USER_DEV@$AWS_IP_DEV "bash -s" < make_dump.sh $DUMP_NAME $AWS_PEM_KEY_STAGING $AWS_USER_STAGING $AWS_IP_STAGING $MYSQL_USER_DEV $MYSQL_PWD_DEV
-  ssh -i $AWS_PEM_KEY_STAGING $AWS_USER_STAGING@$AWS_IP_STAGING "bash -s" < mount_dump.sh $DUMP_NAME $MYSQL_USER_DEV $MYSQL_PWD_DEV
+  # $DUMP_NAME $AWS_PEM_KEY_STAGING $AWS_USER_STAGING $AWS_IP_STAGING $MYSQL_USER_DEV $MYSQL_PWD_DEV
+  ssh -i $AWS_PEM_KEY_DEV $AWS_USER_DEV@$AWS_IP_DEV "mysqldump -u$MYSQL_USER_DEV -p$MYSQL_PWD_DEV openmrs > /var/www/bahmni_config/backup/$DUMP_NAME && sudo scp -i $AWS_PEM_KEY_STAGING /var/www/bahmni_config/backup/$DUMP_NAME $AWS_USER_STAGING@$AWS_IP_STAGING:/var/www/bahmni_config/backup" 
+  # $DUMP_NAME $MYSQL_USER_DEV $MYSQL_PWD_DEV
+  ssh -i $AWS_PEM_KEY_STAGING $AWS_USER_STAGING@$AWS_IP_STAGING "sudo service openmrs stop && mysql -u$MYSQL_USER_DEV -p$MYSQL_PWD_DEV openmrs < /var/www/bahmni_config/backup/$DUMP_NAME && sudo service openmrs start" 
 fi
 if [[ "$BUILDKITE_BRANCH" == "master"  ]]; then
   sudo scp -i $AWS_PEM_KEY_PROD -r ./openelis $AWS_USER_PROD@$AWS_IP_PROD:$AWS_DESTINATION_PATH_PROD | tee -a bk-pipeline.log
