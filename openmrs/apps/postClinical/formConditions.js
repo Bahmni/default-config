ea1c3329fa57ff8b3d999630b2387874a083de60
Cristian Echeverria <cristian.echeverri4@gmail.com>
2017-12-08 16:58:26 -0600

    Se agregó el modulo de PostClínica

---
+++ openmrs/apps/postClinical/formConditions.js
@@ -0,0 +1,28 @@
+Bahmni.ConceptSet.FormConditions.rules = {
+    'Diastolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    },
+    'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    }
+};
