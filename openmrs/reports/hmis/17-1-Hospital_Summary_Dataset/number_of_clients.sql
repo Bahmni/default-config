3ccd8d53afc96d08d5b2feadd8059a7533034531
suruchee <suruchee11@gmail.com>
2018-04-16 11:44:07 +0545

    hospital services error https://app.asana.com/0/0/630872296114277/f

--- openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/number_of_clients.sql
+++ openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/number_of_clients.sql
@@ -11,10 +11,10 @@ FROM
                    person.gender AS patient_gender,
                    observed_age_group.sort_order AS sort_order
    FROM visit
-     INNER JOIN patient ON visit.patient_id = patient.patient_id AND DATE(visit.date_stopped) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) AND patient.voided = 0 AND visit.voided = 0
+     INNER JOIN patient ON visit.patient_id = patient.patient_id AND DATE(visit.date_started) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) AND patient.voided = 0 AND visit.voided = 0
      INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
      RIGHT OUTER JOIN reporting_age_group AS observed_age_group ON
-                                                                  DATE(visit.date_stopped) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+                                                                  DATE(visit.date_started) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
                                                                   AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
    WHERE observed_age_group.report_group_name = 'Client Service Reports') AS client_visits
 GROUP BY client_visits.age_group
