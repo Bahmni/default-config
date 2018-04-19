78ebb0332480d9161e8197397aaf6a6e421f06c1
tbindu <tbindu@thoughtworks.com>
2018-04-19 14:09:56 +0530

    Bindu | BAH-453 | Adding offline config

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,4 +7,4 @@ ROOT_DIR=$BASE_DIR/..
 mkdir -p $ROOT_DIR/target
 rm -rf $ROOT_DIR/target/default_config.zip
 
-cd $ROOT_DIR && zip -r target/default_config.zip openmrs/* migrations/* openelis/*
+cd $ROOT_DIR && zip -r target/default_config.zip openmrs/* migrations/* openelis/* offline/*
