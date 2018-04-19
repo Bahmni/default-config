8a9658704bbdd67a754e8e9889c7db0595c44626
suruchee <suruchee11@gmail.com>
2018-04-19 11:36:08 +0545

    safe motherhood family planning query error resolved-https://app.asana.com/0/0/628768019115536/f

--- openmrs/reports/hmis/06-Safe_Motherhood_Program/Family_planning_Method.sql
+++ openmrs/reports/hmis/06-Safe_Motherhood_Program/Family_planning_Method.sql
@@ -1,13 +1,12 @@
-SELECT 
-  first_answers.category AS 'Category',
-         IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Condoms', 'Pills','Depo Provera') and first_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Short-Term',
-    IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Male sterilization','IUCD','Implant','Female sterilization') and first_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Long-Term'
-FROM
+SELECT DISTINCT
+ first_answers.category AS 'Category',
+         IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Condoms', 'Pills','Depo Provera') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Short-Term',
+    IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Male sterilization','IUCD','Implant','Female sterilization') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Long-Term'
+    FROM
     (SELECT 
         ca.answer_concept AS answer,
             IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name,
-			question_concept_name.name AS category
-
+            question_concept_name.name AS category
     FROM
         concept c
     INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
@@ -17,8 +16,7 @@ FROM
     INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
     INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
         AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
-        		AND answer_concept_fully_specified_name.name NOT IN ('Not Applicable')
-
+        AND answer_concept_fully_specified_name.name NOT IN ('Not Applicable')
         AND answer_concept_fully_specified_name.voided
         IS FALSE
     LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
@@ -26,7 +24,7 @@ FROM
         AND answer_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ('Safe abortion-Surgical procedure','Safe abortion-Medical Procedure')
+        question_concept_name.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) first_answers
         INNER JOIN
@@ -53,34 +51,31 @@ FROM
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) second_answers
         LEFT OUTER JOIN
-    (SELECT DISTINCT
-        DISTINCT(o1.person_id),
+    (SELECT 
+    DISTINCT (o1.person_id),
             cn2.concept_id AS answer,
             cn1.concept_id AS question,
-            v1.visit_id AS visit_id,
-            v1.date_stopped AS datetime
+            v1.visit_id AS visit_id
     FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name IN ('Safe abortion-Surgical procedure','Safe abortion-Medical Procedure')
+        AND cn1.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-        		AND cn2.name NOT IN ('Not Applicable')
+        AND cn2.name NOT IN ('Not Applicable')
         AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
-        AND v1.date_stopped IS NOT NULL
     WHERE
-        CAST(v1.date_stopped AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) first_concept ON first_concept.answer = first_answers.answer
-        LEFT OUTER JOIN
-    (SELECT DISTINCT
-        DISTINCT(o1.person_id),
+        CAST(v1.date_started AS DATE)  BETWEEN DATE('#startDate#') AND DATE('#endDate#'))first_concept ON first_concept.answer = first_answers.answer
+         LEFT OUTER JOIN
+    (SELECT 
+        DISTINCT(o1.person_id) ,
             cn2.concept_id AS answer,
-            v1.visit_id AS visit_id,
-            v1.date_stopped AS datetime
+            v1.visit_id AS visit_id
     FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
@@ -93,11 +88,8 @@ FROM
         AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
-        AND v1.date_stopped IS NOT NULL
     WHERE
-        CAST(v1.date_stopped AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON second_concept.answer = second_answers.answer
+        CAST(v1.date_started AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON second_concept.answer = second_answers.answer
         AND first_concept.person_id = second_concept.person_id
         AND first_concept.visit_id = second_concept.visit_id
-	
-    
-GROUP BY first_answers.category;
+	GROUP BY first_answers.category;
