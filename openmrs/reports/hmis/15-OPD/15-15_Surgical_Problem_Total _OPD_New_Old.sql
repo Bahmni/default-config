0886a4fb26f4f62876b290fb69bbd6cf6d7059eb
suruchee <suruchee11@gmail.com>
2018-04-19 13:41:06 +0545

    opd count problem solved https://app.asana.com/0/0/641062384498015/f

--- openmrs/reports/hmis/15-OPD/15-15_Surgical_Problem_Total _OPD_New_Old.sql
+++ openmrs/reports/hmis/15-OPD/15-15_Surgical_Problem_Total _OPD_New_Old.sql
@@ -1,19 +1,36 @@
-SELECT 
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'F', 1, 0),0))) AS 'Total New OPD Visits Female',
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'M', 1, 0),0))) AS 'Total New OPD Visits Male',
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) - IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'F', 1, 0),0))) as 'Total Old OPD Visits Female' ,
-       IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) - IF(client_visits.patient_id IS NULL, 0, SUM(IF(DATE(client_visits.first_visit_date) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE),IF(client_visits.patient_gender = 'M', 1, 0),0))) AS 'Total Old OPD Visits Male'
 
+
+SELECT 
+    COUNT(DISTINCT IF((gender = 'F' AND Type = 'New'),
+            obs_id,
+            NULL)) AS 'Total New OPD Visits Female',
+    COUNT(DISTINCT IF((gender = 'M' AND Type = 'New'),
+            obs_id,
+            NULL)) AS 'Total New OPD Visits Male',
+    COUNT(DISTINCT IF((gender = 'F' AND Type = 'Returning'),
+            obs_id,
+            NULL)) AS 'Total Old OPD Visits Female',
+    COUNT(DISTINCT IF((gender = 'M' AND Type = 'Returning'),
+            obs_id,
+            NULL)) AS 'Total Old OPD Visits Male'
 FROM
-  (SELECT DISTINCT patient.patient_id AS patient_id,
-                   patient.date_created AS first_visit_date,
-                   person.gender AS patient_gender
-   FROM visit
-     INNER JOIN patient ON visit.patient_id = patient.patient_id AND DATE(visit.date_started) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) AND patient.voided = 0 AND visit.voided = 0
-     INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
-     INNER JOIN visit_attribute va on visit.visit_id = va.visit_id and va.value_reference = 'OPD'
-     INNER JOIN visit_attribute_type vat on vat.visit_attribute_type_id = va.attribute_type_id AND vat.name = 'Visit Status'
-     RIGHT OUTER JOIN reporting_age_group AS observed_age_group ON
-                                                                  DATE(visit.date_started) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
-                                                                  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-   WHERE observed_age_group.report_group_name = 'Client Service Reports') AS client_visits
+    (SELECT 
+        dcv.concept_full_name,
+            dcv.icd10_code,
+            o.obs_id,
+            p.gender,
+            IF(DATE(p.date_created) = DATE(v.date_started), 'New', 'Returning') AS 'Type'
+    FROM
+        person p
+    INNER JOIN visit v ON p.person_id = v.patient_id
+        AND v.voided = 0
+    INNER JOIN encounter e ON v.visit_id = e.visit_id AND e.voided = 0
+    INNER JOIN obs o ON e.encounter_id = o.encounter_id
+        AND o.voided = 0
+        AND DATE(o.obs_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+    INNER JOIN concept_name cn ON cn.concept_id = o.concept_id
+        AND cn.name IN ('Coded Diagnosis' , 'Non-coded Diagnosis')
+    LEFT JOIN diagnosis_concept_view dcv ON dcv.concept_id = o.value_coded
+    WHERE
+        p.voided = 0
+    GROUP BY o.obs_id , dcv.icd10_code) a;
