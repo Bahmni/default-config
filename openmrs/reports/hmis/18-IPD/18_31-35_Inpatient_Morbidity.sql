329e2a82983d0e1cb074fd08ca9b063c0dffe9ee
Dipak Thapa <dipakthapaofficial@gmail.com>
2018-06-18 11:12:25 +0545

    Adds morbidity query

--- openmrs/reports/hmis/18-IPD/18_31-35_Inpatient_Morbidity.sql
+++ openmrs/reports/hmis/18-IPD/18_31-35_Inpatient_Morbidity.sql
@@ -1,41 +1,55 @@
 SELECT 
     first_answers.answer_name AS 'ICD name',
     first_answers.icd10_code AS 'ICD CODE',
-    rag.name AS 'Age Group',
-    IFNULL(SUM(CASE
-                WHEN
-                    second_concept.gender = 'F'
-                        AND second_concept.person_id IS NOT NULL
-                THEN
-                    1
-                ELSE 0
-            END),
-            0) AS 'FEMALE PATIENT',
-    IFNULL(SUM(CASE
-                WHEN
-                    second_concept.gender = 'M'
-                        AND second_concept.person_id IS NOT NULL
-                THEN
-                    1
-                ELSE 0
-            END),
-            0) AS 'MALE PATIENT'
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '≤ 28 Days'  THEN 1 ELSE 0  END), 0) AS 'F ≤ 28 Days',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '≤ 28 Days'  THEN 1 ELSE 0  END), 0) AS 'M ≤ 28 Days',
+
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '29 Days ‐ 1 Year'  THEN 1 ELSE 0  END), 0) AS 'F 29 Days ‐ 1 Year',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '29 Days ‐ 1 Year' THEN 1 ELSE 0  END), 0) AS 'M 29 Days ‐ 1 Year',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '01 ‐ 04 Years'  THEN 1 ELSE 0  END), 0) AS 'F 01 ‐ 04 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '01 ‐ 04 Years'  THEN 1 ELSE 0  END), 0) AS 'M 01 ‐ 04 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '05 ‐ 14 years'  THEN 1 ELSE 0  END), 0) AS 'F 05 ‐ 14 years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '05 ‐ 14 years'  THEN 1 ELSE 0  END), 0) AS 'M 05 ‐ 14 years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '15 ‐ 19 Years'  THEN 1 ELSE 0  END), 0) AS 'F 15 ‐ 19 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '15 ‐ 19 Years'  THEN 1 ELSE 0  END), 0) AS 'M 15 ‐ 19 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '20 ‐ 29 Years'  THEN 1 ELSE 0  END), 0) AS 'F 20 ‐ 29 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '20 ‐ 29 Years'  THEN 1 ELSE 0  END), 0) AS 'M 20 ‐ 29 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '30 ‐ 39 Years'  THEN 1 ELSE 0  END), 0) AS 'F 30 ‐ 39 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '30 ‐ 39 Years'  THEN 1 ELSE 0  END), 0) AS 'M 30 ‐ 39 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '40 ‐ 49 Years'  THEN 1 ELSE 0  END), 0) AS 'F 40 ‐ 49 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '40 ‐ 49 Years'  THEN 1 ELSE 0  END), 0) AS 'M 40 ‐ 49 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '50 ‐ 59 Years'  THEN 1 ELSE 0  END), 0) AS 'F ≤ 28 Days',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '50 ‐ 59 Years'  THEN 1 ELSE 0  END), 0) AS 'M 50 ‐ 59 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '≥ 60 Years'  THEN 1 ELSE 0  END), 0) AS 'F ≥ 60 Years',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.age_grp = '≥ 60 Years'  THEN 1 ELSE 0  END), 0) AS 'M ≥ 60 Years',
+
+IFNULL(SUM(CASE WHEN second_concept.gender = 'F' AND second_concept.person_id IS NOT NULL AND second_concept.is_dead THEN 1 ELSE 0  END), 0) AS 'F Deaths',
+IFNULL(SUM(CASE WHEN second_concept.gender = 'M' AND second_concept.person_id IS NOT NULL AND second_concept.is_dead THEN 1 ELSE 0  END), 0) AS 'M Deaths',
+   combo_id
 FROM
     (SELECT 
         concept_full_name AS answer_name, icd10_code
     FROM
-        diagnosis_concept_view
-    WHERE
-        icd10_code IN ('B07' , 'B15.9', 'B16', 'B17.2', 'B20')) first_answers
+        diagnosis_concept_view 
+ ) first_answers
         INNER JOIN
-    reporting_age_group rag ON rag.report_group_name = 'Inpatient'
-        LEFT OUTER JOIN
-    (SELECT DISTINCT
-        (p.person_id),
+   (SELECT DISTINCT
+        p.person_id,
+        dhis_map.attr_option_combo_id AS combo_id,
             dcv.concept_full_name,
-            icd10_code,
-            v.visit_id AS visit_id,
-            p.gender AS gender
+            p.gender AS gender,
+            dcv.icd10_code,
+            p.birthdate,
+            TIMESTAMPDIFF(YEAR, p.birthdate, v.date_started) AS age
     FROM
         person p
     INNER JOIN visit v ON p.person_id = v.patient_id
@@ -43,42 +57,37 @@ FROM
     INNER JOIN encounter e ON v.visit_id = e.visit_id AND e.voided = 0
     INNER JOIN obs o ON e.encounter_id = o.encounter_id
         AND o.voided = 0
-        AND DATE(o.obs_datetime) BETWEEN '#startDate#' AND '#endDate#'
+        AND o.obs_datetime BETWEEN DATE('#startDate#') AND DATE('#endDate#')
     INNER JOIN concept_name cn ON o.concept_id = cn.concept_id
         AND cn.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn.name IN ('Non-coded Diagnosis' , 'Coded Diagnosis')
+        AND cn.name = 'Coded Diagnosis'
         AND o.voided = 0
         AND cn.voided = 0
-    LEFT JOIN diagnosis_concept_view dcv ON dcv.concept_id = o.value_coded
-        AND dcv.icd10_code IN ('B07' , 'B15.9', 'B16', 'B17.2', 'B20')
+    JOIN diagnosis_concept_view dcv ON dcv.concept_id = o.value_coded
+    JOIN dhis2_icd_map dhis_map on dcv.icd10_code = dhis_map.icd_code
     WHERE
-        p.voided = 0
-    GROUP BY o.obs_id , dcv.icd10_code) first_concept ON first_concept.icd10_code = first_answers.icd10_code
-        LEFT OUTER JOIN
-    (SELECT DISTINCT
-        (person.person_id) AS person_id,
-            obs.value_coded AS answer,
-            obs.concept_id AS question,
-            obs.obs_datetime AS datetime,
-            visit.visit_id AS visit_id,
-            person.gender AS gender
-    FROM
-        obs
-    INNER JOIN concept_view question ON obs.concept_id = question.concept_id
-        AND question.concept_full_name IN ('Discharge note, Inpatient outcome')
-    INNER JOIN person ON obs.person_id = person.person_id
-    INNER JOIN encounter ON obs.encounter_id = encounter.encounter_id
-    INNER JOIN visit ON encounter.visit_id = visit.visit_id
-    WHERE
-        CAST(obs.obs_datetime AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON first_concept.person_id = second_concept.person_id
-        AND first_concept.visit_id = second_concept.visit_id
-        LEFT OUTER JOIN
-    person p ON first_concept.person_id = p.person_id
-        AND CAST(second_concept.datetime AS DATE) BETWEEN (DATE_ADD(DATE_ADD(p.birthdate,
-            INTERVAL rag.min_years YEAR),
-        INTERVAL rag.min_days DAY)) AND (DATE_ADD(DATE_ADD(p.birthdate,
-            INTERVAL rag.max_years YEAR),
-        INTERVAL rag.max_days DAY))
-        AND rag.report_group_name = 'Inpatient'
-GROUP BY first_answers.answer_name , rag.name
-ORDER BY FIELD(first_answers.icd10_code,'B07','B15.9','B16','B17.2','B20') , first_answers.answer_name , rag.sort_order
+        p.voided = 0) first_concept  ON first_concept.icd10_code = first_answers.icd10_code 
+		
+		JOIN 
+		(SELECT DISTINCT
+                person.person_id AS person_id,
+                rag.name AS age_grp,
+                person.gender AS gender,
+                CASE WHEN obs.value_coded IN (SELECT concept_id FROM concept_view WHERE concept_full_name IN ('Death ? 48 Hours', 'Death < 48 Hours'))  THEN true ELSE false END AS is_dead
+            FROM
+                obs 
+            INNER JOIN concept_view question ON obs.concept_id = question.concept_id
+                AND question.concept_full_name = 'Discharge note, Inpatient outcome'
+            INNER JOIN person ON obs.person_id = person.person_id
+            INNER JOIN encounter ON obs.encounter_id = encounter.encounter_id
+            INNER JOIN visit ON encounter.visit_id = visit.visit_id
+			
+			INNER JOIN reporting_age_group rag ON visit.date_started BETWEEN (DATE_ADD(
+                     DATE_ADD(person.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY)) AND (DATE_ADD(
+                     DATE_ADD(person.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
+                                                       AND rag.report_group_name = 'Inpatient'
+            WHERE obs.obs_datetime
+            BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept 
+            ON first_concept.person_id = second_concept.person_id
+GROUP BY first_answers.icd10_code, first_answers.answer_name, combo_id
+ORDER BY first_answers.icd10_code;
