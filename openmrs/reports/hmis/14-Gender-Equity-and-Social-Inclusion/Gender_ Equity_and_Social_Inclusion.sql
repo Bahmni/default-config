b2d5fa94dc06fda709990c3de1b739d137f7a1c6
unknown <laxmanmanandhar7@gmail.com>
2018-06-22 12:55:55 +0545

    14-Gender equity OPD count new fixed

--- openmrs/reports/hmis/14-Gender-Equity-and-Social-Inclusion/Gender_ Equity_and_Social_Inclusion.sql
+++ openmrs/reports/hmis/14-Gender-Equity-and-Social-Inclusion/Gender_ Equity_and_Social_Inclusion.sql
@@ -87,18 +87,20 @@ INNER JOIN
 -- Out patient cases
 (SELECT
 	 caste_list.answer_concept_name AS caste_ethnicity,
-	 SUM(IF(person.gender = 'F', 1, 0)) AS op_cases_female,
-     SUM(IF(person.gender = 'M', 1, 0)) AS op_cases_male
+	 SUM(IF(person.gender = 'M', 1, 0)) as op_cases_male,
+	 SUM(IF(person.gender = 'F', 1, 0)) as op_cases_female
+
 FROM visit
-INNER JOIN person ON visit.patient_id = person.person_id
-	AND DATE(visit.date_stopped) BETWEEN  '#startDate#' AND '#endDate#'
+    INNER JOIN person ON visit.patient_id = person.person_id
+	AND DATE(person.date_created) BETWEEN '#startDate#' AND '#endDate#'
+    AND TIMESTAMPDIFF(YEAR, person.birthdate, visit.date_started) > 5
 INNER JOIN person_attribute ON person_attribute.person_id = person.person_id
 INNER JOIN person_attribute_type ON person_attribute.person_attribute_type_id = person_attribute_type.person_attribute_type_id
 	AND person_attribute_type.name = 'Caste'    
 INNER JOIN encounter ON visit.visit_id = encounter.visit_id
 INNER JOIN coded_obs_view ON encounter.encounter_id = coded_obs_view.encounter_id
 	AND coded_obs_view.concept_full_name IN ('Department Sent To')
-       AND coded_obs_view.value_concept_full_name IN ('OPD')
+       AND upper(coded_obs_view.value_concept_full_name) NOT Like '%EMERGENCY%'
     AND coded_obs_view.voided is FALSE
 RIGHT OUTER JOIN (SELECT answer_concept_name, answer_concept_id FROM concept_answer_view WHERE question_concept_name = 'Caste' ) AS caste_list ON caste_list.answer_concept_id = person_attribute.value
 GROUP BY caste_list.answer_concept_name) AS op_cases ON abortion.caste_ethnicity = op_cases.caste_ethnicity
