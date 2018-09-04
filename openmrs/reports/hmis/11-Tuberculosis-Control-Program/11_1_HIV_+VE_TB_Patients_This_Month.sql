0ccd824908847c0037a25adcbbfc71c3f0a4c6a0
suruchee <suruchee11@gmail.com>
2018-09-04 13:42:25 +0545

    TB related issues resolved

--- openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_HIV_+VE_TB_Patients_This_Month.sql
+++ openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_HIV_+VE_TB_Patients_This_Month.sql
@@ -2,7 +2,7 @@ SELECT
     gender.gender AS 'Gender',
     SUM(CASE
         WHEN
-            first_concept.answer IS NOT NULL
+            first_concept.question IS NOT NULL
                 AND p.gender IS NOT NULL
         THEN
             1
@@ -10,27 +10,20 @@ SELECT
     END) AS 'Patient Count'
 FROM
     (SELECT 
-        ca.answer_concept AS answer,
-            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name
+        question_concept_name.concept_id AS question,
+            IFNULL(question_concept_short_name.name, question_concept_name.name) AS answer_name
     FROM
         concept c
     INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
     INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
         AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
         AND question_concept_name.voided IS FALSE
-    INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
-    INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
-        AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
-        AND answer_concept_fully_specified_name.name IN ('Yes')
-        AND answer_concept_fully_specified_name.voided
-        IS FALSE
-    LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
-        AND answer_concept_short_name.concept_name_type = 'SHORT'
-        AND answer_concept_short_name.voided
+    LEFT JOIN concept_name question_concept_short_name ON question_concept_name.concept_id = question_concept_short_name.concept_id
+        AND question_concept_short_name.concept_name_type = 'SHORT'
+        AND question_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ('Tuberculosis, HIV Infection')
-            AND cd.name = 'Coded'
+        question_concept_name.name IN ('Tuberculosis, Is patient on ART','Tuberculosis, Is patient on CPT')
     ORDER BY answer_name DESC) first_answers
         INNER JOIN
     (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
@@ -39,13 +32,14 @@ FROM
         DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
             cn1.concept_id AS question,
+			cn2.concept_id AS answer_name,
             v1.visit_id AS visit_id,
             v1.date_stopped AS datetime
     FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name = 'Tuberculosis, HIV Infection'
+        AND cn1.name IN ('Tuberculosis, Is patient on ART','Tuberculosis, Is patient on CPT')
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
@@ -56,8 +50,8 @@ FROM
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
         AND v1.date_stopped IS NOT NULL
     WHERE
-        CAST(v1.date_stopped AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) first_concept ON first_concept.answer = first_answers.answer
+        CAST(v1.date_stopped AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) first_concept ON first_concept.question = first_answers.question
         LEFT OUTER JOIN
     person p ON first_concept.person_id = p.person_id
         AND p.gender = gender.gender
-GROUP BY gender.gender;
+GROUP BY  gender.gender;
