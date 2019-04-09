0eec7035f9f11fdc77de4edc26d1a1100990499a
suruchee <suruchee11@gmail.com>
2019-04-09 16:12:30 +0545

    tb cpt and art config

--- openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_HIV_+VE_TB_Patients_ART_CPT_This_Month.sql
+++ openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_HIV_+VE_TB_Patients_ART_CPT_This_Month.sql
@@ -31,9 +31,7 @@ FROM
         LEFT OUTER JOIN
     (SELECT 
         DISTINCT(o1.person_id),
-            cn2.concept_id AS answer,
             cn1.concept_id AS question,
-			cn2.concept_id AS answer_name,
             v1.visit_id AS visit_id,
             v1.date_stopped AS datetime
     FROM
@@ -43,13 +41,10 @@ FROM
         AND cn1.name IN ('Tuberculosis, Is patient on ART','Tuberculosis, Is patient on CPT')
         AND o1.voided = 0
         AND cn1.voided = 0
-    INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
-        AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-		AND cn2.name = 'Yes'
-        AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
     WHERE
+		o1.value_coded = 1 AND
         CAST(e.encounter_datetime AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) first_concept ON first_concept.question = first_answers.question
         LEFT OUTER JOIN
     person p ON first_concept.person_id = p.person_id
--- openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_Tubercluosis_BC_or_CD_Registration_allrelapse.sql
+++ openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_1_Tubercluosis_BC_or_CD_Registration_allrelapse.sql
@@ -76,7 +76,7 @@ FROM
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn2.name IN ('Relapse','Other previously treated', 'Treatment after failure','PNC - Transfer In','Treatment after loss to follow-up','Previous treatment history unknown')
+        AND cn2.name IN ('Relapse','Other previously treated', 'Treatment after failure','Transfer In','Treatment after loss to follow-up','Previous treatment history unknown')
         AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
