e856c5471e98c79757bce99dd6246b0eb5514b27
Amol Chavan <amol@satvix.com>
2019-05-16 12:49:43 +0530

    Amol | Added form condition for MPOC-252

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -12,6 +12,30 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
+    'PP_Key_population' : function (formName, formFieldValues) {
+        var dia = formFieldValues['PP_Key_population'];
+        if (dia ==  "PP_Key_population_Yes") {
+            return {
+                show: ["PP_If_Key_population_yes"]
+            }
+        } else {
+            return {
+                hide: ["PP_If_Key_population_yes"]
+            }
+        }
+     },
+     'PP_Vulnerable_Population' : function (formName, formFieldValues) {
+        var dia = formFieldValues['PP_Vulnerable_Population'];
+        if (dia ==  "PP_Vulnerable_Population_Yes") {
+            return {
+                show: ["PP_IF_Vulnerable_Population_Yes"]
+            }
+        } else {
+            return {
+                hide: ["PP_IF_Vulnerable_Population_Yes"]
+            }
+        }
+     },
     'Systolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
