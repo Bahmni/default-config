8e7fabd91c53398912ab4d02c6698ffc0f0544ee
Simao Mazive <simao.mazive@jembi.org>
2019-06-26 20:29:20 +0200

    work in progress

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -16,5 +16,19 @@ Bahmni.Registration.customValidator = {
             return value.match(/^\w+$/);
         },
         errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
+    },
+    "FATHER_NAME" : {
+        method: function (name, value, personAttributeDetails) {
+            var regexCharacters = "^[a-záàãâéèêẽíìóòõôúùçA-ZÁÀÃÂÉÈÊẼÍÌÓÒÔÕÚÙÇ ]+$";
+            return value.match(regexCharacters);
+        },
+        errorMessage: "REGISTRATION_INVALID_FATHER_NAME_FIELD"
+    },
+    "MOTHER_NAME" : {
+        method: function (name, value, personAttributeDetails) {
+            var regexCharacters = "^[a-záàãâéèêẽíìóòõôúùçA-ZÁÀÃÂÉÈÊẼÍÌÓÒÔÕÚÙÇ ]+$";
+            return value.match(regexCharacters);
+        },
+        errorMessage: "REGISTRATION_INVALID_MOTHER_NAME_FIELD"
     }
 };
