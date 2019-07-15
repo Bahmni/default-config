0485aeb5a8a26f58b312149ffc6739627c4959e5
david mukungi <mukungidavid@gmail.com>
2019-07-15 17:31:46 +0300

    End Of follow up form validation

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -24,5 +24,45 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["Posture"]
             }
         }
-    }
+    },
+    "End Of Follow Up Reason" : function(formName, formFieldValues) {
+        var death = formFieldValues["End Of Follow Up Reason"];       
+        if(death === "Death during treatment") {
+            return {
+                hide: ["Lost To Follow Up Information" , "Transferred Out Information"],
+                show:["Death Information"]
+                // hide:["Transferred Out Information"]
+            }
+        } else if( death === "Lost Follow Up( < 3 Months)"){ 
+            return {
+                hide: ["Death Information", "Transferred Out Information"],
+                show: ["Lost To Follow Up Information"]
+        }
+        }else if (death === "Transferred Out"){
+            return {
+                hide: ["Lost To Follow Up Information" , "Death Information"],
+                show: ["Transferred Out Information"]
+            }
+        }
+        else {
+            return {
+                hide: ["Death Information", "Transferred Out Information" , "Lost To Follow Up Information"]
+            }
+        }
+        
+    }, 
+    "Occupation" : function (formName , formFieldValues){
+        var occupationanswer = formFieldValues["Occupation"];
+       
+        if(occupationanswer === "Occupation - Other (Specify)"){
+          return{
+              show: ["Other - Occupation"]
+          }
+        }else {
+            return{
+                hide: ["Other - Occupation"]
+            }
+        }
+
+    }  
 };
