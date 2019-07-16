45aec12a1fa6008f211f70b84e66defe8e6d3b6d
tbindu <tbindu@thoughtworks.com>
2019-07-16 17:49:39 +0530

    Bindu | MOBN-552 | Added Clinical-App role and fixed the user's family name

--- openmrs/migrations/bedManagement/createBedManagementUserProcedure.sql
+++ openmrs/migrations/bedManagement/createBedManagementUserProcedure.sql
@@ -11,7 +11,7 @@ CREATE PROCEDURE create_bed_management_user( user_gender varchar(50),
     INSERT INTO person (gender, creator, date_created, uuid) VALUES (user_gender, 6, now(), @puuid);
     INSERT INTO person_name (person_id, given_name, family_name,creator, date_created, uuid) VALUES ((SELECT person_id FROM person where uuid=@puuid),user_given_name,user_family_name,6, now(), uuid());
     INSERT INTO users (system_id, username, password, salt, creator, date_created, changed_by, person_id,retired,uuid) VALUES (user_username, user_username, user_password, user_salt, 6,now(),6,(SELECT person_id FROM person where uuid=@puuid),0,uuid());
-    INSERT INTO user_role VALUES ((SELECT max(user_id) FROM users),"Registration-App"),((SELECT max(user_id) FROM users),"InPatient-App");
+    INSERT INTO user_role VALUES ((SELECT max(user_id) FROM users),"Registration-App"),((SELECT max(user_id) FROM users),"InPatient-App"),((SELECT max(user_id) FROM users),"Clinical-App");
     INSERT INTO provider (person_id, creator, date_created, retired, uuid)VALUES ((SELECT person_id FROM person where uuid=@puuid),6,now(),0,uuid());
   END
 #
