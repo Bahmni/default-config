a346947d44676411005641210fc636bb17d460c7
Umair Fayaz <omayrfayaz@gmail.com>
2022-02-07 18:01:57 +0530

    BAH-1380 | Changed zip name to default_config
    Co-authored-by: swatigogia2020 <swati.gogia@thoughtworks.com>

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,6 +5,6 @@ BASE_DIR=`dirname $0`
 ROOT_DIR=$BASE_DIR/..
 
 mkdir -p $ROOT_DIR/target
-rm -rf $ROOT_DIR/target/defaultconfig.zip
+rm -rf $ROOT_DIR/target/default_config.zip
 
-cd $ROOT_DIR && zip -r target/defaultconfig.zip openmrs/* migrations/* openelis/* offline/*
+cd $ROOT_DIR && zip -r target/default_config.zip openmrs/* migrations/* openelis/* offline/*
